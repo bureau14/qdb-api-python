45beb8051b3df8a10350c78316b6f737fa63f178
Edouard Alligand <noone@prout.org>
2013-09-05 22:19:32 +0200

    Case 621 - On FreeBSD make sure we use Python 2.7

--- clang_build_wrap.cmake
+++ clang_build_wrap.cmake
@@ -1,3 +1,3 @@
 set(ENV{CC} clang)
 set(ENV{CXX} clang++)
-execute_process(COMMAND python setup.py build WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+execute_process(COMMAND python2.7 setup.py build WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
