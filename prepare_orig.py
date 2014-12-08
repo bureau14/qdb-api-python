f709999aef38d0fc182f0f30fa3322f985ad5075
Benoit Blanchon <benoit@quasardb.net>
2014-12-08 18:09:32 +0100

    Case 749 - Make windows api dll more "static"

--- prepare_orig.py
+++ prepare_orig.py
@@ -11,30 +11,9 @@ dirs_to_build = [package_include_dir, 'src', 'qdb']
 ''' parameters given by CMake '''
 header_files = ["@QDB_API_C_HEADER@", "@QDB_API_CPP_HEADER@"]
 library_dir = "@ACTUAL_LIBRARY_PATH@"
-cpp_libs = "@QDB_CPP_LIBS@".split(';')
 package_modules = ['@QDB_API_DLL@'] 
 
-# cpp_libs can be empty don't add an empty list
-if len(cpp_libs) > 0:
-    package_modules.extend(cpp_libs)
-
 python_modules = ['@QDB_PY_IMPL@', '@QDB_PY_INTERFACE@']
-
-tbb = '@TBB_SHARED@'
-if len(tbb) > 0:
-    package_modules.append(tbb)
-
-tbb_malloc = '@TBB_MALLOC_SHARED@'
-if len(tbb_malloc) > 0:
-    package_modules.append(tbb_malloc)
-
-malloc_proxy = '@TBB_MALLOC_PROXY_SHARED@'
-if len(malloc_proxy) > 0:
-    package_modules.append(malloc_proxy)
-
-malloc_proxy = '@TBB_MALLOC_PROXY_SHARED@'
-if len(malloc_proxy) > 0:
-    package_modules.append(malloc_proxy)
 
 for d in dirs_to_build:
     if not os.path.exists(d):
