387c00b80acd8a050654d8beea8905d117629eb6
Remi Marty <remi@quasardb.net>
2018-12-06 17:51:05 +0100

    Made the C API check unittest clearer

--- testcases/test_cluster.py
+++ testcases/test_cluster.py
@@ -3,13 +3,11 @@ import re
 import unittest
 from settings import quasardb
 
-def make_invalid_cluster():
-    return quasardb.Cluster("_qdb_://invaliduri")
-
 class QuasardbCluster(unittest.TestCase):
     def test_capi_version_check():
         # This will throw everytime
         # The version check is the most critical step so it is done first.
         # We thus just have to check that the error we get is related to
         # this check to figure how it went.
-        self.assertRaisesRegexp(quasardb.Error, "^((?!C API version).)*$", make_invalid_cluster)
+        self.assertRaisesRegexp(quasardb.Error, "^((?!C API version).)*$",
+            quasardb.Cluster, "_qdb_://invaliduri")
