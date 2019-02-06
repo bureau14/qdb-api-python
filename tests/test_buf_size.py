0be200c11ca4889d41989789651fa887b1ff9b28
Leon Mergen <leon@solatis.com>
2019-02-06 15:29:34 +0100

    Refactor buf size tests to pytest

--- tests/test_buf_size.py
+++ tests/test_buf_size.py
@@ -1,49 +1,18 @@
 # pylint: disable=C0103,C0111,C0302,W0212
-import datetime
-import os
-import sys
-import unittest
-import settings
 
-from settings import quasardb
+import quasardb
+import pytest
 
-class QuasardbOptionBufSize(unittest.TestCase):
+def test_set_client_max_in_buf_1(qdbd_connection):
+    with pytest.raises(quasardb.Error):
+        qdbd_connection.options().set_client_max_in_buf_size(1)
 
-    def test_set_client_max_in_buf_1(self):
-         self.assertRaises(quasardb.Error,
-                          settings.cluster.options().set_client_max_in_buf_size, 1)
+def test_set_client_max_in_buf_1MB(qdbd_connection):
+    qdbd_connection.options().set_client_max_in_buf_size(1024 * 1024)
+    assert qdbd_connection.options().get_client_max_in_buf_size() == (1024 * 1024)
 
-    def test_set_client_max_in_buf_1MB(self):
-        try:
-            settings.cluster.options().set_client_max_in_buf_size(1024 * 1024)
-            self.assertEqual(1024 * 1024, settings.cluster.options().get_client_max_in_buf_size())
-        except:  # pylint: disable=W0702
-            self.fail(
-                msg='set/get client max buf should not have raised an exception')
+def test_get_client_max_in_buf(qdbd_connection):
+    assert qdbd_connection.options().get_client_max_in_buf_size() > 0
 
-    def test_get_client_max_in_buf(self):
-        try:
-            self.assertTrue(0 < settings.cluster.options().get_client_max_in_buf_size())
-        except:  # pylint: disable=W0702
-            self.fail(
-                msg='cluster.option().get_client_max_in_buf_size should not have raised an exception')
-
-    def test_get_cluster_max_in_buf(self):
-        try:
-            self.assertTrue(0 < settings.cluster.options().get_cluster_max_in_buf_size())
-        except:  # pylint: disable=W0702
-            self.fail(
-                msg='cluster.option().get_cluster_max_in_buf_size should not have raised an exception')
-
-
-
-if __name__ == '__main__':
-    if settings.get_lock_status() == False:
-        settings.init()
-        test_directory = os.getcwd()
-        test_report_directory = os.path.join(os.path.split(
-            __file__)[0], '..', 'build', 'test', 'test-reports')
-        import xmlrunner
-        unittest.main(testRunner=xmlrunner.XMLTestRunner(  # pylint: disable=E1102
-            output=test_report_directory), exit=False)
-        settings.terminate()
+def test_get_cluster_max_in_buf(qdbd_connection):
+    assert qdbd_connection.options().get_cluster_max_in_buf_size() > 0
