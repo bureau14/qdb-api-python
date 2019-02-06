c9c9518a5d77275eea62baa541b7761a1c6cdf0f
Leon Mergen <leon@solatis.com>
2019-02-06 16:12:15 +0100

    Refactor prefix test to pytest

--- tests/test_prefix.py
+++ tests/test_prefix.py
@@ -1,38 +1,25 @@
 # pylint: disable=C0103,C0111,C0302,W0212
-import os
-import unittest
-import settings
 
-class QuasardbPrefix(unittest.TestCase):
-    def test_empty_prefix(self):
+def test_empty_prefix(qdbd_connection):
 
-        res = settings.cluster.prefix_get("testazeazeaze", 10)
-        self.assertEqual(len(res), 0)
+    res = qdbd_connection.prefix_get("testazeazeaze", 10)
+    assert len(res) == 0
 
-        self.assertEqual(0, settings.cluster.prefix_count("testazeazeaze"))
+    res = qdbd_connection.prefix_count("testazeazeaze")
+    assert res == 0
 
-    def test_find_one(self):
-        dat_prefix = "my_dark_prefix"
-        entry_name = dat_prefix + settings.entry_gen.next()
-        entry_content = "content"
+def test_find_one(qdbd_connection, entry_name, random_string):
+    dat_prefix = random_string
 
-        b = settings.cluster.blob(entry_name)
-        b.put(entry_content)
-
-        res = settings.cluster.prefix_get(dat_prefix, 10)
-        self.assertEqual(len(res), 1)
-        self.assertEqual(res[0], entry_name)
-
-        self.assertEqual(1, settings.cluster.prefix_count(dat_prefix))
+    entry_name = dat_prefix + entry_name
+    entry_content = "content"
 
 
-if __name__ == '__main__':
-    if settings.get_lock_status() is False:
-        settings.init()
-        test_directory = os.getcwd()
-        test_report_directory = os.path.join(os.path.split(
-            __file__)[0], '..', 'build', 'test', 'test-reports')
-        import xmlrunner
-        unittest.main(testRunner=xmlrunner.XMLTestRunner(  # pylint: disable=E1102
-            output=test_report_directory), exit=False)
-        settings.terminate()
+    b = qdbd_connection.blob(entry_name)
+    b.put(entry_content)
+
+    res = qdbd_connection.prefix_get(dat_prefix, 10)
+    assert len(res) == 1
+    assert res[0] == entry_name
+
+    assert qdbd_connection.prefix_count(dat_prefix) == 1
