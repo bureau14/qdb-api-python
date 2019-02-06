a08e54a3736f20843845d144d0b8147ba499e6de
Leon Mergen <leon@solatis.com>
2019-02-06 17:16:13 +0100

    Refactor query find tests

--- tests/test_query_find.py
+++ tests/test_query_find.py
@@ -1,74 +1,39 @@
 # pylint: disable=C0103,C0111,C0302,W0212
-import os
-import unittest
-import settings
+import pytest
+import quasardb
 
-class QuasardbQueryFind(unittest.TestCase):
+def test_types(qdbd_connection, blob_entry, random_blob, tag_name):
 
-    def test_types(self):
+    blob_entry.put(random_blob)
+    blob_entry.attach_tag(tag_name)
 
-        my_tag = "my_tag" + settings.entry_gen.next()
+    res = qdbd_connection.find("find(tag='" + tag_name + "')").run()
 
-        entry_name = settings.entry_gen.next()
-        entry_content = "content"
+    assert len(res) == 1
 
-        b = settings.cluster.blob(entry_name)
-        b.put(entry_content)
-        b.attach_tag(my_tag)
+    res = qdbd_connection.find(
+        "find(tag='" + tag_name + "' AND type=blob)").run()
 
-        res = settings.cluster.find("find(tag='" + my_tag + "')").run()
-
-        self.assertEqual(len(res), 1)
-        self.assertEqual(res[0], entry_name)
-
-        res = settings.cluster.find(
-            "find(tag='" + my_tag + "' AND type=blob)").run()
-
-        self.assertEqual(len(res), 1)
-        self.assertEqual(res[0], entry_name)
-
-        res = settings.cluster.find(
-            "find(tag='" + my_tag + "' AND type=integer)").run()
-
-        self.assertEqual(len(res), 0)
-
-    def test_two_tags(self):
-
-        tag1 = "tag1" + settings.entry_gen.next()
-        tag2 = "tag2" + settings.entry_gen.next()
-
-        entry_name = settings.entry_gen.next()
-        entry_content = "content"
-
-        b = settings.cluster.blob(entry_name)
-        b.put(entry_content)
-        b.attach_tag(tag1)
-        b.attach_tag(tag2)
-
-        res = settings.cluster.find("find(tag='" + tag1 + "')").run()
-
-        self.assertEqual(len(res), 1)
-        self.assertEqual(res[0], entry_name)
-
-        res = settings.cluster.find("find(tag='" + tag2 + "')").run()
-
-        self.assertEqual(len(res), 1)
-        self.assertEqual(res[0], entry_name)
-
-        res = settings.cluster.find(
-            "find(tag='" + tag1 + "' AND tag='" + tag2 + "')").run()
-
-        self.assertEqual(len(res), 1)
-        self.assertEqual(res[0], entry_name)
+    assert len(res) == 1
 
 
-if __name__ == '__main__':
-    if settings.get_lock_status() is False:
-        settings.init()
-        test_directory = os.getcwd()
-        test_report_directory = os.path.join(os.path.split(
-            __file__)[0], '..', 'build', 'test', 'test-reports')
-        import xmlrunner
-        unittest.main(testRunner=xmlrunner.XMLTestRunner(  # pylint: disable=E1102
-            output=test_report_directory), exit=False)
-        settings.terminate()
+    res = qdbd_connection.find(
+        "find(tag='" + tag_name + "' AND type=integer)").run()
+
+    assert len(res) == 0
+
+def test_types(qdbd_connection, blob_entry, random_blob, tag_names):
+    blob_entry.put(random_blob)
+    blob_entry.attach_tags(tag_names)
+
+
+    res = qdbd_connection.find("find(tag='" + tag_names[0] + "')").run()
+    assert len(res) == 1
+
+    res = qdbd_connection.find("find(tag='" + tag_names[1] + "')").run()
+    assert len(res) == 1
+
+    res = qdbd_connection.find(
+        "find(tag='" + tag_names[0] + "' AND tag='" + tag_names[1] + "')").run()
+
+    assert len(res) == 1
