920d5c9e784b48804830d679aa18ec479cca4997
Leon Mergen <leon@solatis.com>
2019-02-06 17:10:34 +0100

    Refactors compression tests to pytest

--- tests/test_setCompression.py
+++ tests/test_setCompression.py
@@ -1,38 +1,14 @@
 # pylint: disable=C0103,C0111,C0302,W0212,W0702
-import os
-import sys
-import unittest
-import settings
 
-from settings import quasardb
+import pytest
+import quasardb
 
-class QuasardbClusterSetCompression(unittest.TestCase):
-    def test_compression_none(self):
-        try:
-            settings.cluster.options().set_compression(quasardb.Options.Compression.Disabled)
-        except:
-            self.fail(
-                msg='cluster.set_compression should not have raised an exception')
+def test_compression_none(qdbd_connection):
+    qdbd_connection.options().set_compression(quasardb.Options.Compression.Disabled)
 
-    def test_compression_fast(self):
-        try:
-            settings.cluster.options().set_compression(quasardb.Options.Compression.Fast)
-        except:
-            self.fail(
-                msg='cluster.set_compression should not have raised an exception')
+def test_compression_fast(qdbd_connection):
+    qdbd_connection.options().set_compression(quasardb.Options.Compression.Fast)
 
-    def test_compression_invalid(self):
-        self.assertRaises(TypeError,
-                          settings.cluster.options().set_compression, 123)
-
-
-if __name__ == '__main__':
-    if settings.get_lock_status() is False:
-        settings.init()
-        test_directory = os.getcwd()
-        test_report_directory = os.path.join(os.path.split(
-            __file__)[0], '..', 'build', 'test', 'test-reports')
-        import xmlrunner
-        unittest.main(testRunner=xmlrunner.XMLTestRunner(  # pylint: disable=E1102
-            output=test_report_directory), exit=False)
-        settings.terminate()
+def test_compression_invalid(qdbd_connection):
+    with pytest.raises(TypeError):
+        qdbd_connection.options().set_compression(123)
