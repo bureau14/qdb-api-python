d0b9766c87a2e28517c13bd9c1776560f85965fb
Leon Mergen <leon@solatis.com>
2019-02-06 15:36:12 +0100

    Refactors suffix tests to pytest

--- tests/test_suffix.py
+++ tests/test_suffix.py
@@ -1,39 +1,24 @@
 # pylint: disable=C0103,C0111,C0302,W0212
-import os
-import unittest
-import settings
 
-class QuasardbSuffix(unittest.TestCase):
-    def test_empty_suffix(self):
+def test_empty_suffix(qdbd_connection):
 
-        res = settings.cluster.suffix_get("testazeazeaze", 10)
-        self.assertEqual(len(res), 0)
+    res = qdbd_connection.suffix_get("testazeazeaze", 10)
+    assert len(res) == 0
 
-        self.assertEqual(0, settings.cluster.suffix_count("testazeazeaze"))
+    res = qdbd_connection.suffix_count("testazeazeaze")
+    assert res == 0
 
-    def test_find_one(self):
-        dat_suffix = "my_dark_suffix"
+def test_find_one(qdbd_connection, entry_name, random_string):
+    dat_suffix = random_string
 
-        entry_name = settings.entry_gen.next() + dat_suffix
-        entry_content = "content"
+    entry_name = entry_name + dat_suffix
+    entry_content = "content"
 
-        b = settings.cluster.blob(entry_name)
-        b.put(entry_content)
+    b = qdbd_connection.blob(entry_name)
+    b.put(entry_content)
 
-        res = settings.cluster.suffix_get(dat_suffix, 10)
-        self.assertEqual(len(res), 1)
-        self.assertEqual(res[0], entry_name)
+    res = qdbd_connection.suffix_get(dat_suffix, 10)
+    assert len(res) == 1
+    assert res[0] == entry_name
 
-        self.assertEqual(1, settings.cluster.suffix_count(dat_suffix))
-
-
-if __name__ == '__main__':
-    if settings.get_lock_status() is False:
-        settings.init()
-        test_directory = os.getcwd()
-        test_report_directory = os.path.join(os.path.split(
-            __file__)[0], '..', 'build', 'test', 'test-reports')
-        import xmlrunner
-        unittest.main(testRunner=xmlrunner.XMLTestRunner(  # pylint: disable=E1102
-            output=test_report_directory), exit=False)
-        settings.terminate()
+    assert qdbd_connection.suffix_count(dat_suffix) == 1
