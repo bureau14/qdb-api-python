b16effcb67e0e7b84959c81adb577843e7554760
Leon Mergen <leon@solatis.com>
2019-02-06 14:10:57 +0100

    Migrate to pytest

--- tests/test_cluster.py
+++ tests/test_cluster.py
@@ -1,12 +1,11 @@
 import re
-import unittest
+import pytest
 import quasardb
 
-class QuasardbCluster(unittest.TestCase):
-    def test_capi_version_check(self):
-        # This will throw everytime
-        # The version check is the most critical step so it is done first.
-        # We thus just have to check that the error we get is related to
-        # this check to figure how it went.
-        self.assertRaisesRegex(quasardb.Error, "^((?!C API version).)*$",
-            quasardb.Cluster, "_qdb_://invaliduri")
+def test_capi_version_check():
+    # This will throw everytime
+    # The version check is the most critical step so it is done first.
+    # We thus just have to check that the error we get is related to
+    # this check to figure how it went.
+    with pytest.raises(quasardb.Error, match=r'^((?!C API version).)*$'):
+        quasardb.Cluster("_qdb_://invaliduri")
