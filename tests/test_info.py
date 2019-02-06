71205bba3a25d2fa93d244201e7d9e2e7170748a
Leon Mergen <leon@solatis.com>
2019-02-06 15:26:04 +0100

    Refactor info tests for pytest

--- tests/test_info.py
+++ tests/test_info.py
@@ -1,35 +1,22 @@
 # pylint: disable=C0103,C0111,C0302,W0212
-import os
-import unittest
-import settings
+import quasardb
 
-class QuasardbInfo(unittest.TestCase):
-    def test_node_status(self):
-        status = settings.cluster.node_status(settings.INSECURE_URI)
-        self.assertGreater(len(status), 0)
-        self.assertIsNotNone(status.get('overall'))
+def test_node_status(qdbd_connection, qdbd_settings):
+    status = qdbd_connection.node_status(qdbd_settings.get('uri').get('insecure'))
+    assert len(status) > 0
+    assert status.get('overall') != None
 
-    def test_node_config(self):
-        config = settings.cluster.node_config(settings.INSECURE_URI)
-        self.assertGreater(len(config), 0)
-        self.assertIsNotNone(config.get('global'))
-        self.assertIsNotNone(config.get('local'))
+def test_node_config(qdbd_connection, qdbd_settings):
+    config = qdbd_connection.node_config(qdbd_settings.get('uri').get('insecure'))
+    assert len(config) > 0
+    assert config.get('global') != None
+    assert config.get('local') != None
 
-    def test_node_topology(self):
-        topology = settings.cluster.node_topology(settings.INSECURE_URI)
-        self.assertGreater(len(topology), 0)
-        self.assertIsNotNone(topology.get('predecessor'))
-        self.assertIsNotNone(topology.get('center'))
-        self.assertIsNotNone(topology.get('successor'))
+def test_node_topology(qdbd_connection, qdbd_settings):
+    topology = qdbd_connection.node_topology(qdbd_settings.get('uri').get('insecure'))
 
+    assert len(topology) > 0
 
-if __name__ == '__main__':
-    if settings.get_lock_status() is False:
-        settings.init()
-        test_directory = os.getcwd()
-        test_report_directory = os.path.join(os.path.split(
-            __file__)[0], '..', 'build', 'test', 'test-reports')
-        import xmlrunner
-        unittest.main(testRunner=xmlrunner.XMLTestRunner(  # pylint: disable=E1102
-            output=test_report_directory), exit=False)
-        settings.terminate()
+    assert topology.get('predecessor') != None
+    assert topology.get('center') != None
+    assert topology.get('successor') != None
