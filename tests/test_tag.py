b080e03433396a137ea0a201f44c8cb15d36a76c
Leon Mergen <leon@solatis.com>
2019-02-06 17:06:56 +0100

    Refactors tag tests to pytest

--- tests/test_tag.py
+++ tests/test_tag.py
@@ -1,96 +1,37 @@
 # pylint: disable=C0103,C0111,C0302,W0212
-import os
-import unittest
-import settings
 
-class QuasardbTag(unittest.TestCase):
+def test_get_entries(blob_entry, random_blob, tag_name):
+    blob_entry.put(random_blob)
 
-    def test_get_entries(self):
-        entry_name = settings.entry_gen.next()
-        entry_content = "content"
+    tags = blob_entry.get_tags()
+    assert len(tags) == 0
 
-        tag_name = settings.entry_gen.next()
+    assert blob_entry.has_tag(tag_name) == False
+    assert blob_entry.attach_tag(tag_name) == True
+    assert blob_entry.attach_tag(tag_name) == False
 
-        b = settings.cluster.blob(entry_name)
+    tags = blob_entry.get_tags()
+    assert len(tags) == 1
+    assert tags[0] == tag_name
 
-        b.put(entry_content)
+    assert blob_entry.has_tag(tag_name) == True
+    assert blob_entry.detach_tag(tag_name) == True
+    assert blob_entry.detach_tag(tag_name) == False
 
-        tags = b.get_tags()
-        self.assertEqual(0, len(tags))
+    tags = blob_entry.get_tags()
+    assert len(tags) == 0
 
-        self.assertFalse(b.has_tag(tag_name))
+def test_tag_multiple(blob_entry, random_blob, tag_names):
+    blob_entry.put(random_blob)
 
-        self.assertTrue(b.attach_tag(tag_name))
-        self.assertFalse(b.attach_tag(tag_name))
+    tags = blob_entry.get_tags()
+    assert len(tags) == 0
 
-        tags = b.get_tags()
-        self.assertEqual(1, len(tags))
-        self.assertEqual(tags[0], tag_name)
+    blob_entry.attach_tags(tag_names)
 
-        self.assertTrue(b.has_tag(tag_name))
+    assert sorted(blob_entry.get_tags()) == tag_names
 
-        self.assertTrue(b.detach_tag(tag_name))
-        self.assertFalse(b.detach_tag(tag_name))
+    blob_entry.detach_tags(tag_names)
 
-        tags = b.get_tags()
-        self.assertEqual(0, len(tags))
-
-    def test_tag_multiple(self):
-        entry_name = settings.entry_gen.next()
-        entry_content = "content"
-
-        tags_to_add = sorted([settings.entry_gen.next() for i in range(0, 10)])
-
-        b = settings.cluster.blob(entry_name)
-
-        b.put(entry_content)
-
-        tags = b.get_tags()
-        self.assertEqual(0, len(tags))
-
-        b.attach_tags(tags_to_add)
-
-        self.assertEqual(tags_to_add, sorted(b.get_tags()))
-
-        b.detach_tags(tags_to_add)
-
-        tags = b.get_tags()
-        self.assertEqual(0, len(tags))
-
-    def test_tag_sequence(self):
-        entry_name = settings.entry_gen.next()
-        entry_content = "content"
-
-        tag_name = settings.entry_gen.next()
-
-        b = settings.cluster.blob(entry_name)
-        b.put(entry_content)
-
-        t = settings.cluster.tag(tag_name)
-
-        entries = t.get_entries()
-        self.assertEqual(0, len(entries))
-
-        self.assertTrue(b.attach_tag(tag_name))
-
-        tags = t.get_entries()
-        self.assertEqual(1, len(tags))
-        self.assertEqual(tags[0], entry_name)
-
-        self.assertEqual(1, t.count())
-
-        self.assertTrue(b.detach_tag(tag_name))
-        entries = t.get_entries()
-        self.assertEqual(0, len(entries))
-
-
-if __name__ == '__main__':
-    if settings.get_lock_status() is False:
-        settings.init()
-        test_directory = os.getcwd()
-        test_report_directory = os.path.join(os.path.split(
-            __file__)[0], '..', 'build', 'test', 'test-reports')
-        import xmlrunner
-        unittest.main(testRunner=xmlrunner.XMLTestRunner(  # pylint: disable=E1102
-            output=test_report_directory), exit=False)
-        settings.terminate()
+    tags = blob_entry.get_tags()
+    assert len(tags) == 0
