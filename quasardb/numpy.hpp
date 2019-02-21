709e0d1020a7204c44e1a85a5c6ac798b76ef251
Leon Mergen <leon@solatis.com>
2019-02-21 18:15:49 +0100

    Formats source code according to code style

--- quasardb/numpy.hpp
+++ quasardb/numpy.hpp
@@ -30,10 +30,8 @@
  */
 #pragma once
 
-#include <pybind11/numpy.h>
-
 #include "ts_convert.hpp"
-
+#include <pybind11/numpy.h>
 
 /**
  * A datetime64 in numpy is modeled as a scalar array, which is not integrated
@@ -52,99 +50,99 @@
 
 // Begin numpy proxy
 
-typedef enum {
-  /* Force signed enum type, must be -1 for code compatibility */
-  NPY_FR_ERROR = -1,      /* error or undetermined */
+typedef enum
+{
+    /* Force signed enum type, must be -1 for code compatibility */
+    NPY_FR_ERROR = -1, /* error or undetermined */
 
-  /* Start of valid units */
-  NPY_FR_Y = 0,           /* Years */
-  NPY_FR_M = 1,           /* Months */
-  NPY_FR_W = 2,           /* Weeks */
-  /* Gap where 1.6 NPY_FR_B (value 3) was */
-  NPY_FR_D = 4,           /* Days */
-  NPY_FR_h = 5,           /* hours */
-  NPY_FR_m = 6,           /* minutes */
-  NPY_FR_s = 7,           /* seconds */
-  NPY_FR_ms = 8,          /* milliseconds */
-  NPY_FR_us = 9,          /* microseconds */
-  NPY_FR_ns = 10,         /* nanoseconds */
-  NPY_FR_ps = 11,         /* picoseconds */
-  NPY_FR_fs = 12,         /* femtoseconds */
-  NPY_FR_as = 13,         /* attoseconds */
-  NPY_FR_GENERIC = 14     /* unbound units, can convert to anything */
+    /* Start of valid units */
+    NPY_FR_Y = 0, /* Years */
+    NPY_FR_M = 1, /* Months */
+    NPY_FR_W = 2, /* Weeks */
+    /* Gap where 1.6 NPY_FR_B (value 3) was */
+    NPY_FR_D       = 4,  /* Days */
+    NPY_FR_h       = 5,  /* hours */
+    NPY_FR_m       = 6,  /* minutes */
+    NPY_FR_s       = 7,  /* seconds */
+    NPY_FR_ms      = 8,  /* milliseconds */
+    NPY_FR_us      = 9,  /* microseconds */
+    NPY_FR_ns      = 10, /* nanoseconds */
+    NPY_FR_ps      = 11, /* picoseconds */
+    NPY_FR_fs      = 12, /* femtoseconds */
+    NPY_FR_as      = 13, /* attoseconds */
+    NPY_FR_GENERIC = 14  /* unbound units, can convert to anything */
 } NPY_DATETIMEUNIT;
 
-typedef struct {
-  NPY_DATETIMEUNIT base;
-  int num;
+typedef struct
+{
+    NPY_DATETIMEUNIT base;
+    int num;
 } PyArray_DatetimeMetaData;
 
 typedef std::int64_t npy_datetime;
 
-typedef struct {
-  PyObject_HEAD
-  npy_datetime obval;
-  PyArray_DatetimeMetaData obmeta;
+typedef struct
+{
+    PyObject_HEAD npy_datetime obval;
+    PyArray_DatetimeMetaData obmeta;
 } PyDatetimeScalarObject;
 
 // End numpy proxy
-
 
 namespace py = pybind11;
 
 namespace qdb
 {
-  class numpy {
+namespace numpy
+{
 
-  public:
-    typedef PyDatetimeScalarObject py_datetime_t;
+typedef PyDatetimeScalarObject py_datetime_t;
 
-    /**
-     * Convert nanoseconds int64 to a numpy datetime
-     */
-    inline static py::handle to_datetime64(std::int64_t ts) {
-      py_datetime_t * res = _new_datetime64();
+/**
+ * Convert nanoseconds int64 to a numpy datetime
+ */
+inline static py::handle to_datetime64(std::int64_t ts)
+{
+    py_datetime_t * res = _new_datetime64();
 
-      res->obmeta.num = 1; // version?
-      res->obmeta.base = NPY_FR_ns; // our timestamps are always in ns
-      res->obval = ts;
+    res->obmeta.num  = 1;         // version?
+    res->obmeta.base = NPY_FR_ns; // our timestamps are always in ns
+    res->obval       = ts;
 
-      return reinterpret_cast<PyObject *>(res);
-    }
-
-    /**
-     * Convenience function, directly convert a qdb_timespec to numpy
-     * datetime PyObject
-     */
-    inline static py::handle to_datetime64(const qdb_timespec_t & ts) {
-      return to_datetime64(convert_timestamp(ts));
-    }
-
-
-  private:
-
-    inline static PyTypeObject * _get_datetime64_type() noexcept {
-      /**
-       * Note that this type is not shipped with pybind by default, and we made
-       * local modifications to the pybind/numpy.hpp code to look up this
-       * type at runtime.
-       */
-      return py::detail::npy_api::get().PyDatetimeArrType_;
-    }
-
-    /**
-     * Allocate a new numpy.datetime64 python object. This invokes the numpy code
-     * dynamically loaded at runtime.
-     */
-    inline static py_datetime_t * _new_datetime64() {
-      PyTypeObject * type = _get_datetime64_type();
-
-      // Allocate memory
-      py_datetime_t * res =
-        reinterpret_cast<py_datetime_t *>(type->tp_alloc(type, 1));
-
-      // Call constructor.
-      return PyObject_INIT_VAR(res, type, sizeof(py_datetime_t));
-    }
-  };
+    return reinterpret_cast<PyObject *>(res);
 }
+
+/**
+ * Convenience function, directly convert a qdb_timespec to numpy
+ * datetime PyObject
+ */
+inline static py::handle to_datetime64(const qdb_timespec_t & ts)
+{
+    return to_datetime64(convert_timestamp(ts));
+}
+
+private:
+inline static PyTypeObject * _get_datetime64_type() noexcept
+{
+    // Note that this type is not shipped with pybind by default, and we made
+    // local modifications to the pybind/numpy.hpp code to look up this
+    // type at runtime.
+    return py::detail::npy_api::get().PyDatetimeArrType_;
+}
+
+/**
+ * Allocate a new numpy.datetime64 python object. This invokes the numpy code
+ * dynamically loaded at runtime.
+ */
+inline static py_datetime_t * _new_datetime64()
+{
+    PyTypeObject * type = _get_datetime64_type();
+
+    // Allocate memory
+    py_datetime_t * res = reinterpret_cast<py_datetime_t *>(type->tp_alloc(type, 1));
+
+    // Call constructor.
+    return PyObject_INIT_VAR(res, type, sizeof(py_datetime_t));
+}
+}; // namespace numpy
+} // namespace qdb
--- quasardb/ts_reader.hpp
+++ quasardb/ts_reader.hpp
@@ -30,244 +30,246 @@
  */
 #pragma once
 
-#include <iostream>
+#include "numpy.hpp"
+#include "ts_convert.hpp"
 #include <qdb/ts.h>
 #include <pybind11/numpy.h>
-
-#include "ts_convert.hpp"
-#include "numpy.hpp"
+#include <iostream>
 
 namespace py = pybind11;
 
 namespace qdb
 {
 
-  typedef std::vector<qdb_ts_column_info_t> ts_columns_t;
+typedef std::vector<qdb_ts_column_info_t> ts_columns_t;
 
-
-  /**
-   * Our value class points to a specific index in a local table, and provides
-   * the necessary conversion functions. It does not hold any value of itself.
-   */
-  class ts_reader_value {
-  public:
-    ts_reader_value() :
-      _local_table(nullptr),
-      _index(-1),
-      _type (qdb_ts_column_uninitialized)
-    {
-    }
+/**
+ * Our value class points to a specific index in a local table, and provides
+ * the necessary conversion functions. It does not hold any value of itself.
+ */
+class ts_reader_value
+{
+public:
+    ts_reader_value()
+        : _local_table(nullptr)
+        , _index(-1)
+        , _type(qdb_ts_column_uninitialized)
+    {}
 
     ts_reader_value(qdb_local_table_t local_table, int64_t index, qdb_ts_column_type_t type)
-      :  _local_table(local_table),
-         _index(index),
-         _type(type) {
-    }
-
+        : _local_table(local_table)
+        , _index(index)
+        , _type(type)
+    {}
 
     /**
      * Coerce this value to a Python value, used by the automatic type caster
      * defined at the bottom.
      */
-    py::handle
-    cast() const {
-      switch (_type) {
-      case qdb_ts_column_double:
-        return _double();
-      case qdb_ts_column_blob:
-        return _blob();
-      case qdb_ts_column_int64:
-        return _int64();
-      case qdb_ts_column_timestamp:
-        return _timestamp();
-      };
+    py::handle cast() const
+    {
+        switch (_type)
+        {
+        case qdb_ts_column_double:
+            return _double();
+        case qdb_ts_column_blob:
+            return _blob();
+        case qdb_ts_column_int64:
+            return _int64();
+        case qdb_ts_column_timestamp:
+            return _timestamp();
+        };
 
-      throw std::runtime_error("Unable to cast QuasarDB type to Python type");
+        throw std::runtime_error("Unable to cast QuasarDB type to Python type");
     }
 
-  private:
-    py::handle
-    _int64() const noexcept {
-      std::int64_t v;
-      qdb::qdb_throw_if_error(qdb_ts_row_get_int64(_local_table, _index, &v));
-      return PyLong_FromLongLong(v);
+private:
+    py::handle _int64() const noexcept
+    {
+        std::int64_t v;
+        qdb::qdb_throw_if_error(qdb_ts_row_get_int64(_local_table, _index, &v));
+        return PyLong_FromLongLong(v);
     }
 
-    py::handle
-    _blob() const noexcept {
-      void const * v = nullptr;
-      qdb_size_t l = 0;
+    py::handle _blob() const noexcept
+    {
+        void const * v = nullptr;
+        qdb_size_t l   = 0;
 
-      qdb::qdb_throw_if_error(qdb_ts_row_get_blob(_local_table, _index, &v, &l));
-      return PyByteArray_FromStringAndSize(static_cast<char const *>(v),
-                                           static_cast<Py_ssize_t>(l));
+        qdb::qdb_throw_if_error(qdb_ts_row_get_blob(_local_table, _index, &v, &l));
+        return PyByteArray_FromStringAndSize(static_cast<char const *>(v), static_cast<Py_ssize_t>(l));
     }
 
-    py::handle
-    _double() const noexcept {
-      double v = 0.0;
-      qdb::qdb_throw_if_error(qdb_ts_row_get_double(_local_table, _index, &v));
-      return PyFloat_FromDouble(v);
+    py::handle _double() const noexcept
+    {
+        double v = 0.0;
+        qdb::qdb_throw_if_error(qdb_ts_row_get_double(_local_table, _index, &v));
+        return PyFloat_FromDouble(v);
     }
 
-    py::handle
-    _timestamp() const noexcept {
-      qdb_timespec_t v;
-      qdb::qdb_throw_if_error(qdb_ts_row_get_timestamp(_local_table, _index, &v));
+    py::handle _timestamp() const noexcept
+    {
+        qdb_timespec_t v;
+        qdb::qdb_throw_if_error(qdb_ts_row_get_timestamp(_local_table, _index, &v));
 
-      return qdb::numpy::to_datetime64(v);
+        return qdb::numpy::to_datetime64(v);
     }
 
-  private:
+private:
     qdb_local_table_t _local_table;
     int64_t _index;
     qdb_ts_column_type_t _type;
-  };
+};
 
-  /**
-   * Our row class is nothing more than an interface on top of the local
-   * table api. It allows lazy access (and possible conversion) of the objects, and
-   * as such avoids copies.
-   */
-  class ts_row {
-  public:
-
+/**
+ * Our row class is nothing more than an interface on top of the local
+ * table api. It allows lazy access (and possible conversion) of the objects, and
+ * as such avoids copies.
+ */
+class ts_row
+{
+public:
     // We need a default constructor to due being copied as part of an iterator.
-    ts_row() :
-      _local_table(nullptr) {
+    ts_row()
+        : _local_table(nullptr)
+    {}
+
+    ts_row(qdb_local_table_t local_table, ts_columns_t columns)
+        : _local_table(local_table)
+        , _columns(columns)
+    {}
+
+    bool operator==(ts_row const & rhs) const noexcept
+    {
+        // Since our row doesn't hold any intrinsic data itself and is merely
+        // an indirection to the data in the local table, it doesn't make a lot
+        // of sense to compare it with another other than comparing the timestamps
+        // and the local table references.
+        return _timestamp.tv_sec == rhs._timestamp.tv_sec && _timestamp.tv_nsec == rhs._timestamp.tv_nsec
+               && _local_table == rhs._local_table;
     }
 
-    ts_row(qdb_local_table_t local_table, ts_columns_t columns) :
-      _local_table(local_table),
-      _columns(columns) {
-    }
-
-    bool operator==(ts_row const & rhs) const noexcept {
-      /**
-       * Since our row doesn't hold any intrinsic data itself and is merely
-       * an indirection to the data in the local table, it doesn't make a lot
-       * of sense to compare it with another other than comparing the timestamps
-       * and the local table references.
-       */
-      return
-        _timestamp.tv_sec == rhs._timestamp.tv_sec &&
-        _timestamp.tv_nsec == rhs._timestamp.tv_nsec &&
-        _local_table == rhs._local_table;
-    }
-
-    py::handle
-    timestamp() {
-      return numpy::to_datetime64(_timestamp);
+    py::handle timestamp()
+    {
+        return numpy::to_datetime64(_timestamp);
     }
 
     /**
      * Not exposed through Python, but allows the read_row operation to write the underlying
      * timestamp object directly.
      */
-    qdb_timespec_t &
-    mutable_timestamp() {
-      return _timestamp;
+    qdb_timespec_t & mutable_timestamp()
+    {
+        return _timestamp;
     }
 
-    ts_reader_value
-    get_item(int64_t index) {
-      // TODO: we construct this object every time; should be without any heap allocations,
-      // but can we do without this?
-      return ts_reader_value(_local_table, index, _columns.at(index).type);
+    ts_reader_value get_item(int64_t index)
+    {
+        // TODO: we construct this object every time; should be without any heap allocations,
+        // but can we do without this?
+        return ts_reader_value(_local_table, index, _columns.at(index).type);
     }
 
-    void
-    set_item(int64_t index, int64_t value) {
-      // not implemented
+    void set_item(int64_t index, int64_t value)
+    {
+        // not implemented
     }
 
-  private:
+private:
     qdb_local_table_t _local_table;
     ts_columns_t _columns;
     qdb_timespec_t _timestamp;
-  };
+};
 
-  class ts_reader_iterator {
-  public:
+class ts_reader_iterator
+{
+public:
     using value_type        = ts_row;
     using difference_type   = std::ptrdiff_t;
-    using pointer           = const value_type*;
-    using reference         = const value_type&;
+    using pointer           = const value_type *;
+    using reference         = const value_type &;
     using iterator_category = std::forward_iterator_tag;
 
-  public:
+public:
     ts_reader_iterator()
-      : _local_table(nullptr),
-        _the_row(_local_table, _columns) {
-    }
+        : _local_table(nullptr)
+        , _the_row(_local_table, _columns)
+    {}
 
     ts_reader_iterator(qdb_local_table_t local_table, ts_columns_t columns)
-      : _local_table(local_table),
-        _columns (columns),
-        _the_row(_local_table, _columns) {
-
-      // Work around the api wanting us to go 'next' to go to the beginning
-      ++(*this);
+        : _local_table(local_table)
+        , _columns(columns)
+        , _the_row(_local_table, _columns)
+    {
+        // Immediately try to move to the first row
+        ++(*this);
     }
 
-    bool operator==(ts_reader_iterator const& rhs) const noexcept {
-      // Our .end() iterator is recognized by a null local table.
-      if (rhs._local_table == nullptr || _local_table == nullptr) {
-        return _local_table == rhs._local_table;
-      } else {
-        return _the_row == rhs._the_row;
-      }
+    bool operator==(ts_reader_iterator const & rhs) const noexcept
+    {
+        // Our .end() iterator is recognized by a null local table.
+        if (rhs._local_table == nullptr || _local_table == nullptr)
+        {
+            return _local_table == rhs._local_table;
+        }
+        else
+        {
+            return _the_row == rhs._the_row;
+        }
     }
-    bool operator!=(ts_reader_iterator const& rhs) const noexcept {
-      return !(*this == rhs);
-    }
-
-    pointer operator->() const noexcept {
-      return &_the_row;
-    }
-
-    reference operator*() const noexcept {
-      return _the_row;
+    bool operator!=(ts_reader_iterator const & rhs) const noexcept
+    {
+        return !(*this == rhs);
     }
 
-    ts_reader_iterator & operator++() noexcept {
-      qdb_error_t err = qdb_ts_table_next_row(_local_table, &_the_row.mutable_timestamp());
-
-      if (err == qdb_e_iterator_end) {
-        // As seen in the default constructor and operator==, an empty _local_table
-        // designates an end-iterator.
-        _local_table = nullptr;
-      } else {
-        qdb::qdb_throw_if_error(err);
-      }
-
-      return *this;
+    pointer operator->() const noexcept
+    {
+        return &_the_row;
     }
 
-  private:
+    reference operator*() const noexcept
+    {
+        return _the_row;
+    }
+
+    ts_reader_iterator & operator++() noexcept
+    {
+        qdb_error_t err = qdb_ts_table_next_row(_local_table, &_the_row.mutable_timestamp());
+
+        if (err == qdb_e_iterator_end)
+        {
+            // As seen in the default constructor and operator==, an empty _local_table
+            // designates an end-iterator.
+            _local_table = nullptr;
+        }
+        else
+        {
+            qdb::qdb_throw_if_error(err);
+        }
+
+        return *this;
+    }
+
+private:
     qdb_local_table_t _local_table;
     ts_columns_t _columns;
     value_type _the_row;
-  };
+};
 
 class ts_reader
 {
 public:
-  typedef ts_reader_iterator iterator;
+    typedef ts_reader_iterator iterator;
 
 public:
-  ts_reader(qdb::handle_ptr h, const std::string & t, const ts_columns_t & c, const std::vector<qdb_ts_range_t> & r)
-    : _handle{h},
-      _columns{c},
-      _local_table(nullptr)
+    ts_reader(qdb::handle_ptr h, const std::string & t, const ts_columns_t & c, const std::vector<qdb_ts_range_t> & r)
+        : _handle{h}
+        , _columns{c}
+        , _local_table(nullptr)
     {
-      qdb::qdb_throw_if_error(qdb_ts_local_table_init(*_handle,
-                                                      t.c_str(),
-                                                      c.data(),
-                                                      c.size(),
-                                                      &_local_table));
+        qdb::qdb_throw_if_error(qdb_ts_local_table_init(*_handle, t.c_str(), c.data(), c.size(), &_local_table));
 
-      qdb::qdb_throw_if_error(qdb_ts_table_get_ranges(_local_table, r.data(), r.size()));
+        qdb::qdb_throw_if_error(qdb_ts_table_get_ranges(_local_table, r.data(), r.size()));
     }
 
     // since our reader models a stateful generator, we prevent copies
@@ -282,14 +284,14 @@ public:
         }
     }
 
-    iterator
-    begin() {
-      return iterator(_local_table, _columns);
+    iterator begin()
+    {
+        return iterator(_local_table, _columns);
     }
 
-    iterator
-    end() {
-      return iterator();
+    iterator end()
+    {
+        return iterator();
     }
 
 private:
@@ -300,71 +302,60 @@ private:
 
 using ts_reader_ptr = std::unique_ptr<ts_reader>;
 
-
 template <typename Module>
 static inline void register_ts_reader(Module & m)
 {
-  py::class_<qdb::ts_reader_value>{m, "TimeSeriesValue"}
-     ;
+    py::class_<qdb::ts_reader_value>{m, "TimeSeriesValue"};
 
-  py::class_<qdb::ts_row>{m, "TimeSeriesRow"}
-     .def("__getitem__", &qdb::ts_row::get_item, py::return_value_policy::move)
-     .def("__setitem__", &qdb::ts_row::set_item)
-     .def("timestamp", &qdb::ts_row::timestamp)
-     .def("dtest", &qdb::ts_row::dtest)
-     ;
+    py::class_<qdb::ts_row>{m, "TimeSeriesRow"}
+        .def("__getitem__", &qdb::ts_row::get_item, py::return_value_policy::move)
+        .def("__setitem__", &qdb::ts_row::set_item)
+        .def("timestamp", &qdb::ts_row::timestamp)
+        .def("dtest", &qdb::ts_row::dtest);
 
-  py::class_<qdb::ts_reader>{m, "TimeSeriesReader"}
-     .def(py::init<qdb::handle_ptr,
-          const std::string &,
-          const std::vector<qdb_ts_column_info_t> &,
-          const std::vector<qdb_ts_range_t> &>())
+    py::class_<qdb::ts_reader>{m, "TimeSeriesReader"}
+        .def(py::init<qdb::handle_ptr, const std::string &, const std::vector<qdb_ts_column_info_t> &,
+            const std::vector<qdb_ts_range_t> &>())
 
-
-     .def("__iter__", [](ts_reader & r) {
-                        return py::make_iterator(r.begin(), r.end());
-                      },py::keep_alive<0, 1>());
+        .def("__iter__", [](ts_reader & r) { return py::make_iterator(r.begin(), r.end()); }, py::keep_alive<0, 1>());
 }
 
 } // namespace qdb
 
+namespace pybind11
+{
+namespace detail
+{
 
-
-namespace pybind11 {
-  namespace detail {
+/**
+ * Implements custom type caster for our ts_reader_value class, so that conversion
+ * to and from native python types is completely transparent.
+ */
+template <>
+struct type_caster<qdb::ts_reader_value>
+{
+public:
+    /**
+     * Note that this macro magically sets a member variable called 'value'.
+     */
+    PYBIND11_TYPE_CASTER(qdb::ts_reader_value, _("qdb::ts_reader_value"));
 
     /**
-     * Implementsa custom type caster for our ts_reader_value class, so that conversion
-     * to and from native python types is completely transparent.
+     * We do not support Python->C++
      */
-
-    template <> struct type_caster<qdb::ts_reader_value> {
-    public:
-
-      /**
-       * Note that this macro magically sets a member variable called 'value'.
-       */
-      PYBIND11_TYPE_CASTER(qdb::ts_reader_value, _("qdb::ts_reader_value"));
-
-      /**
-       * We do not support Python->C++
-       */
-      bool load(handle src, bool) {
+    bool load(handle src, bool)
+    {
         return false;
-      }
+    }
 
-      /**
-       * C++->Python
-       */
-      static handle cast(qdb::ts_reader_value src,
-                         return_value_policy /* policy */,
-                         handle /* parent */) {
+    /**
+     * C++->Python
+     */
+    static handle cast(qdb::ts_reader_value src, return_value_policy /* policy */, handle /* parent */)
+    {
         return src.cast();
-      }
-
-    };
-
-
-
-  };
+    }
 };
+
+}; // namespace detail
+}; // namespace pybind11
