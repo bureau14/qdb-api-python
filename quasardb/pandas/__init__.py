7b5c8bbb53f742270ed453bd29e623c2a8f90f34
Leon Mergen <leon@solatis.com>
2019-03-04 15:07:26 +0100

    Retrieve columns as pandas timeseries

---
+++ quasardb/pandas/__init__.py
@@ -0,0 +1,54 @@
+# pylint: disable=C0103,C0111,C0302,R0903
+
+# Copyright (c) 2009-2019, quasardb SAS
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#    * Neither the name of quasardb nor the names of its contributors may
+#      be used to endorse or promote products derived from this software
+#      without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY QUASARDB AND CONTRIBUTORS ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+import numpy as np
+import quasardb
+
+class PandasRequired(ImportError):
+    pass
+
+try:
+    from pandas.core.api import DataFrame, Series
+    from pandas.core.base import PandasObject
+except ImportError:
+    raise PandasRequired("The pandas library is required to handle pandas data formats")
+
+def as_series(table, col_name, ranges):
+    dispatch = {
+        quasardb.ColumnType.Double: table.double_get_ranges,
+        quasardb.ColumnType.Blob: table.blob_get_ranges,
+        quasardb.ColumnType.Int64: table.int64_get_ranges,
+        quasardb.ColumnType.Timestamp: table.timestamp_get_ranges,
+    }
+
+    # Dispatch based on column type
+    t = table.column_type_by_id(col_name)
+    res = (dispatch[t])(col_name, ranges)
+
+    return Series(res[1], index=res[0])
---
+++ tests/test_pandas.py
@@ -0,0 +1,34 @@
+# pylint: disable=C0103,C0111,C0302,W0212
+import pytest
+import quasardb.pandas as qdbpd
+from functools import reduce
+import numpy as np
+import pandas as pd
+import test_ts_batch as batchlib
+
+def test_can_read_series(qdbd_connection, table, many_intervals):
+    batch_inserter = qdbd_connection.ts_batch(
+        batchlib._make_ts_batch_info(table))
+
+    doubles, blobs, integers, timestamps = batchlib._test_with_table(
+        batch_inserter,
+        table,
+        many_intervals,
+        batchlib._row_insertion_method,
+        batchlib._regular_push)
+
+    total_range = (many_intervals[0], many_intervals[-1] + np.timedelta64(1, 's'))
+    double_series = qdbpd.as_series(table, "the_double", [total_range])
+    blob_series = qdbpd.as_series(table, "the_blob", [total_range])
+    int64_series = qdbpd.as_series(table, "the_int64", [total_range])
+    ts_series = qdbpd.as_series(table, "the_ts", [total_range])
+
+    assert type(double_series) == pd.core.series.Series
+    assert type(blob_series) == pd.core.series.Series
+    assert type(int64_series) == pd.core.series.Series
+    assert type(ts_series) == pd.core.series.Series
+
+    np.testing.assert_array_equal(double_series.to_numpy(), doubles)
+    np.testing.assert_array_equal(blob_series.to_numpy(), blobs)
+    np.testing.assert_array_equal(int64_series.to_numpy(), integers)
+    np.testing.assert_array_equal(ts_series.to_numpy(), timestamps)
