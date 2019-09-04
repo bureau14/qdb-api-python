c42cba42ea153d4068076abe0ec779db2ecfcc66
Leon Mergen <leon@solatis.com>
2019-09-04 14:08:55 +0200

    Add connect and secure-connect samples

--- examples/tutorial/python.py
+++ examples/tutorial/python.py
@@ -1,3 +1,15 @@
 # import-start
 import quasardb
 # import-end
+
+# connect-start
+c = quasardb.Cluster("qdb://127.0.0.1:28360")
+# connect-end
+
+def secure_connect():
+    # secure-connect-start
+    sc = quasardb.Cluster(uri='qdb://127.0.0.1:2836',
+                          user_name='user_name',
+                          user_private_key='SL8sm9dM5xhPE6VNhfYY4ib4qk3vmAFDXCZ2FDi8AuJ4=',
+                          cluster_public_key='PZMBhqk43w+HNr9lLGe+RYq+qWZPrksFWMF1k1UG/vwc=')
+    # secure-connect-end
