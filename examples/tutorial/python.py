a876303d7cb9166c7905fee920e2563b25bcdb4d
Leon Mergen <leon@solatis.com>
2019-09-24 11:33:44 +0200

    QDB-2705 Add create table example

--- examples/tutorial/python.py
+++ examples/tutorial/python.py
@@ -13,3 +13,18 @@ def secure_connect():
                           user_private_key='SL8sm9dM5xhPE6VNhfYY4ib4qk3vmAFDXCZ2FDi8AuJ4=',
                           cluster_public_key='PZMBhqk43w+HNr9lLGe+RYq+qWZPrksFWMF1k1UG/vwc=')
     # secure-connect-end
+
+# create-table-start
+
+# First we acquire a reference to a table (which may or may not yet exist)
+t = c.table("stocks")
+
+# Initialize our column definitions
+cols = [quasardb.ColumnInfo(quasardb.ColumnType.Double, "open"),
+        quasardb.ColumnInfo(quasardb.ColumnType.Double, "close"),
+        quasardb.ColumnInfo(quasardb.ColumnType.Int64, "volume")]
+
+# Now create the table with the default shard size
+t.create(cols)
+
+# create-table-end
