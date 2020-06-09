a0ff96a99cd2c47975a4f2570a2342ae799c803c
Leon Mergen <leon@solatis.com>
2020-06-09 16:46:21 +0200

    Always use an explicit python path

--- scripts/test/smoke-test.sh
+++ scripts/test/smoke-test.sh
@@ -2,25 +2,14 @@
 
 set -eux -o pipefail
 
-function detect_pipenv() {
-    if [[ ! "${PYTHON_EXECUTABLE-}" == "" ]]
-    then
-        echo "${PYTHON_EXECUTABLE} -m pipenv"
-    else
-        echo $(which pipenv)
-    fi
-}
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-PIPENV=$(detect_pipenv)
-
 FILE=${1}
 
 rm -f ./Pipfile ./Pipfile.lock
 
-${PIPENV} clean
-${PIPENV} install ${FILE}
-${PIPENV} run ${PYTHON_EXECUTABLE} ${DIR}/smoke-test.py
-${PIPENV} uninstall quasardb
+${PYTHON_EXECUTABLE} -m pipenv clean
+${PYTHON_EXECUTABLE} -m pipenv install ${FILE}
+${PYTHON_EXECUTABLE} -m pipenv run ${PYTHON_EXECUTABLE} ${DIR}/smoke-test.py
+${PYTHON_EXECUTABLE} -m pipenv uninstall quasardb
 
 rm -f ./Pipfile ./Pipfile.lock
