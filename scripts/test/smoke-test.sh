9d4dc75f44a00c5a942b3975ec2014e24d8c5cb8
Leon Mergen <leon@solatis.com>
2020-06-09 17:08:06 +0200

    Use python venv instead of pipenv

--- scripts/test/smoke-test.sh
+++ scripts/test/smoke-test.sh
@@ -4,12 +4,9 @@ set -eux -o pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 FILE=${1}
+PYTHON="${PYTHON_EXECUTABLE:-python}"
 
-rm -f ./Pipfile ./Pipfile.lock
-
-${PYTHON_EXECUTABLE} -m pipenv clean
-${PYTHON_EXECUTABLE} -m pipenv install ${FILE}
-${PYTHON_EXECUTABLE} -m pipenv run ${PYTHON_EXECUTABLE} ${DIR}/smoke-test.py
-${PYTHON_EXECUTABLE} -m pipenv uninstall quasardb
-
-rm -f ./Pipfile ./Pipfile.lock
+${PYTHON} -m venv .env/
+source .env/bin/activate
+${PYTHON} -m pip install ${FILE}
+${PYTHON} ${DIR}/smoke-test.py
