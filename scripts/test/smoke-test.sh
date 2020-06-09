28e68682db3397e4370ee558e97973bdf7ea12d0
Leon Mergen <leon@solatis.com>
2020-06-09 17:32:00 +0200

    Remove old venv

--- scripts/test/smoke-test.sh
+++ scripts/test/smoke-test.sh
@@ -6,7 +6,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 FILE=${1}
 PYTHON="${PYTHON_EXECUTABLE:-python}"
 
+rm -rf .env/ || true
+
 ${PYTHON} -m venv .env/
 source .env/bin/activate
 ${PYTHON} -m pip install ${FILE}
 ${PYTHON} ${DIR}/smoke-test.py
+
+rm -rf .env/ || true
