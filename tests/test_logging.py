a949189276d21a91faacf593439f8c23efda07f3
Leon Mergen <leon@solatis.com>
2020-07-13 12:41:14 +0200

    Adds test case which triggers invalid UTF-8 logs

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -3,6 +3,7 @@ import pytest
 import quasardb
 import logging
 import time
+import numpy as np
 
 def test_native_logging_output(blob_entry, random_string, caplog):
     caplog.set_level(logging.DEBUG)
@@ -26,3 +27,46 @@ def test_native_logging_output(blob_entry, random_string, caplog):
 
     assert 'quasardb.native' in modules
     assert 'requested clients broker service run' in messages
+
+def test_invalid_utf8_logs_qdb3361(qdbd_connection, caplog):
+    caplog.set_level(logging.DEBUG)
+
+    # Generate invalid UTF-8 binary string which we'll use as table name
+    tablename = b'foo\x80abc'
+
+    # Verify this does, in fact, raise an error in case when it's interpreted
+    # as UTF-8.
+    with pytest.raises(UnicodeDecodeError):
+        tablename.decode("utf-8")
+
+    # Now, use this tablename to do some batch insertion
+    table = qdbd_connection.table(tablename)
+    try:
+        table.remove()
+    except quasardb.Error:
+        # AliasNotFound
+        pass
+
+    col = quasardb.ColumnInfo(quasardb.ColumnType.Double, "the_double")
+    table.create([col])
+
+    batchcol = [quasardb.BatchColumnInfo(tablename, "the_double", 10)]
+    inserter = qdbd_connection.inserter(batchcol)
+
+    inserter.start_row(np.datetime64('2020-01-01', 'ns'))
+    inserter.set_double(0, 1.234)
+
+    inserter.push()
+    time.sleep(4)
+
+    # This call right here triggers the 'bad' logs to be flushed into the python
+    # userspace.
+    table.remove()
+
+    # Look at the captured log messages to verify our 'broken' utf-8 code is, in fact,
+    # in the logs.
+    seen = False
+    for lr in caplog.records:
+        seen = seen or '\x80' in lr.message
+
+    assert seen == True
