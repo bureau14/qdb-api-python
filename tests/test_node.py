cd3ee6ebe6e1227560793ed090d3409cf1304854
Marek Kurdej <marek@quasardb.net>
2020-11-10 14:44:16 +0100

    QDB-9174 - [API] Internal error when using direct API with wrong URI.

--- tests/test_node.py
+++ tests/test_node.py
@@ -1,4 +1,14 @@
 import pytest
+import quasardb
+
+
+def test_node_wrong_leading_qdb_in_uri(qdbd_connection, qdbd_settings):
+    with pytest.raises(quasardb.Error, match=r'The (handle|argument) is invalid'):
+        # With check in connect, node() throws.
+        direct_conn = qdbd_connection.node(
+            qdbd_settings.get("uri").get("insecure"))
+        # Without it, C API should return an error in prefix_get.
+        direct_conn.prefix_get("$qdb", 10)
 
 
 def test_node_empty_prefix(qdbd_direct_connection):
