6faba71a9b4f0663123fc3030e7b8226998c6b0b
Sidney Congard <sidney.congard@gmail.com>
2020-11-23 12:00:04 +0100

    split test_table.py file

---
+++ tests/test_table_blob.py
@@ -0,0 +1,103 @@
+# pylint: disable=C0103,C0111,C0302,W0212
+import datetime
+import pytest
+import numpy as np
+import quasardb
+import test_table as tslib
+
+
+def test_blob_get_ranges__when_timeseries_is_empty(table, intervals):
+    results = table.blob_get_ranges(tslib._blob_col_name(table), intervals)
+    assert len(results) == 2
+    assert len(results[0]) == 0
+    assert len(results[1]) == 0
+
+
+def test_blob_erase_ranges__when_timeseries_is_empty(table, intervals):
+    erased_count = table.erase_ranges(tslib._blob_col_name(table), intervals)
+    assert erased_count == 0
+
+
+def test_blob_get_ranges(table, intervals):
+    start_time = tslib._start_time(intervals)
+    column_name = tslib._blob_col_name(table)
+
+    inserted_blob_data = tslib._generate_blob_ts(start_time, 1000)
+    table.blob_insert(column_name,
+                      inserted_blob_data[0],
+                      inserted_blob_data[1])
+
+    results = table.blob_get_ranges(column_name, [(
+        start_time, start_time + np.timedelta64(10, 's'))])
+
+    tslib._check_ts_results(results, inserted_blob_data, 10)
+
+    results = table.blob_get_ranges(column_name,
+                                    [(start_time,
+                                      start_time + np.timedelta64(10, 's')),
+                                     (start_time + np.timedelta64(10, 's'),
+                                      start_time + np.timedelta64(20, 's'))])
+
+    tslib._check_ts_results(results, inserted_blob_data, 20)
+
+    # Everything
+    results = table.blob_get_ranges(column_name)
+    tslib._check_ts_results(results, inserted_blob_data, 1000)
+
+    # empty result
+    out_of_time = start_time + np.timedelta64(10, 'h')
+    results = table.blob_get_ranges(_blob_col_name(
+        table), [(out_of_time, out_of_time + np.timedelta64(10, 's'))])
+    assert len(results) == 2
+    assert len(results[0]) == 0
+    assert len(results[1]) == 0
+
+    # error: column doesn't exist
+    with pytest.raises(quasardb.Error):
+        table.blob_get_ranges("lolilol", [(_start_time(
+            intervals), start_time + np.timedelta64(10, 's'))])
+
+    with pytest.raises(quasardb.Error):
+        table.blob_insert(
+            "lolilol",
+            inserted_blob_data[0],
+            inserted_blob_data[1])
+
+    with pytest.raises(TypeError):
+        table.int64_get_ranges(_blob_col_name, [(_start_time(
+            intervals), start_time + np.timedelta64(10, 's'))])
+
+    with pytest.raises(TypeError):
+        table.int64_insert(
+            column_name,
+            inserted_blob_data[0],
+            inserted_blob_data[1])
+
+
+def test_blob_erase_ranges(table, intervals):
+    start_time = tslib._start_time(intervals)
+    column_name = tslib._blob_col_name(table)
+
+    inserted_blob_data = tslib._generate_blob_ts(start_time, 1000)
+    table.blob_insert(
+        column_name,
+        inserted_blob_data[0],
+        inserted_blob_data[1])
+
+    results = table.blob_get_ranges(column_name, [(
+        start_time, start_time + np.timedelta64(10, 's'))])
+
+    erased_count = table.erase_ranges(column_name, [(
+        start_time, start_time + np.timedelta64(10, 's'))])
+
+    assert erased_count == len(results[0])
+
+    erased_count = table.erase_ranges(column_name, [(
+        start_time, start_time + np.timedelta64(10, 's'))])
+
+    assert erased_count == 0
+
+    results = table.blob_get_ranges(column_name, [(
+        start_time, start_time + np.timedelta64(10, 's'))])
+
+    assert len(results[0]) == 0
---
+++ tests/test_table_double.py
@@ -0,0 +1,101 @@
+# pylint: disable=C0103,C0111,C0302,W0212
+import datetime
+import pytest
+import numpy as np
+import quasardb
+import test_table as tslib
+
+
+def test_double_get_ranges__when_timeseries_is_empty(table, intervals):
+    results = table.double_get_ranges(_double_col_name(table), intervals)
+    assert len(results) == 2
+    assert len(results[0]) == 0
+    assert len(results[1]) == 0
+
+
+def test_double_erase_ranges__when_timeseries_is_empty(table, intervals):
+    erased_count = table.erase_ranges(_double_col_name(table), intervals)
+    assert erased_count == 0
+
+
+def test_double_get_ranges(table, intervals):
+    start_time = tslib._start_time(intervals)
+    column_name = tslib._double_col_name(table)
+
+    inserted_double_data = tslib._generate_double_ts(start_time, 1000)
+    table.double_insert(column_name,
+                        inserted_double_data[0],
+                        inserted_double_data[1])
+
+    results = table.double_get_ranges(column_name, [(
+        start_time, start_time + np.timedelta64(10, 's'))])
+
+    tslib._check_ts_results(results, inserted_double_data, 10)
+
+    results = table.double_get_ranges(
+        column_name,
+        [
+            (start_time,
+             start_time + np.timedelta64(10, 's')),
+            (start_time + np.timedelta64(10, 's'),
+             start_time + np.timedelta64(20, 's'))])
+
+    tslib._check_ts_results(results, inserted_double_data, 20)
+
+    # empty result
+    out_of_time = start_time + np.timedelta64(10, 'h')
+    results = table.double_get_ranges(_double_col_name(
+        table), [(out_of_time, out_of_time + np.timedelta64(10, 's'))])
+    assert len(results) == 2
+    assert len(results[0]) == 0
+    assert len(results[1]) == 0
+
+    # error: column doesn't exist
+    with pytest.raises(quasardb.Error):
+        table.double_get_ranges("lolilol", [(_start_time(
+            intervals), start_time + np.timedelta64(10, 's'))])
+
+    with pytest.raises(quasardb.Error):
+        table.double_insert(
+            "lolilol",
+            inserted_double_data[0],
+            inserted_double_data[1])
+
+    with pytest.raises(TypeError):
+        table.blob_get_ranges(_double_col_name, [(_start_time(
+            intervals), start_time + np.timedelta64(10, 's'))])
+
+    with pytest.raises(quasardb.IncompatibleTypeError):
+        table.blob_insert(
+            column_name,
+            inserted_double_data[0],
+            inserted_double_data[1])
+
+
+def test_double_erase_ranges(table, intervals):
+    start_time = tslib._start_time(intervals)
+    column_name = tslib._double_col_name(table)
+
+    inserted_double_data = tslib._generate_double_ts(start_time, 1000)
+    table.double_insert(
+        column_name,
+        inserted_double_data[0],
+        inserted_double_data[1])
+
+    results = table.double_get_ranges(column_name, [(
+        start_time, start_time + np.timedelta64(10, 's'))])
+
+    erased_count = table.erase_ranges(column_name, [(
+        start_time, start_time + np.timedelta64(10, 's'))])
+
+    assert erased_count == len(results[0])
+
+    erased_count = table.erase_ranges(column_name, [(
+        start_time, start_time + np.timedelta64(10, 's'))])
+
+    assert erased_count == 0
+
+    results = table.double_get_ranges(column_name, [(
+        start_time, start_time + np.timedelta64(10, 's'))])
+
+    assert len(results[0]) == 0
