e6d63b45ddb180282deeb98df9378b189531a7c6
vianney <vianney.p@outlook.com>
2021-01-21 16:33:33 +0100

    Simplify overload.

--- quasardb/overload.hpp
+++ quasardb/overload.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#if defined(__cpp_deduction_guides)
-
 template <class... Ts>
 struct overload : Ts...
 {
@@ -12,39 +10,3 @@ struct overload : Ts...
 template <class... Ts>
 overload(Ts...) -> overload<Ts...>;
 
-#else
-
-template <typename... Ts>
-struct overload_set;
-
-template <typename T, typename... Ts>
-struct overload_set<T, Ts...>
-    : T
-    , overload_set<Ts...>
-{
-    overload_set(T t, Ts... ts)
-        : T(t)
-        , overload_set<Ts...>(ts...)
-    {}
-
-    using T::operator();
-    using overload_set<Ts...>::operator();
-};
-
-template <typename T>
-struct overload_set<T> : T
-{
-    overload_set(T t)
-        : T(t)
-    {}
-
-    using T::operator();
-};
-
-template <typename... Ts>
-overload_set<Ts...> overload(Ts... ts)
-{
-    return overload_set<Ts...>(ts...);
-}
-
-#endif
