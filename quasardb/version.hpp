72d6dab63456420cb10aa367911a743db099c36d
terngkub <terngnattapol@gmail.com>
2021-02-04 13:50:29 +0100

    Update copyright year.

--- quasardb/version.hpp
+++ quasardb/version.hpp
@@ -2,7 +2,7 @@
  *
  * Official Python API
  *
- * Copyright (c) 2009-2020, quasardb SAS. All rights reserved.
+ * Copyright (c) 2009-2021, quasardb SAS. All rights reserved.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
