ce2de5eff46cbb27d14d73be278d72edc130b3f8
vianney <vianney.p@outlook.com>
2021-03-26 09:09:52 +0100

    [fix] Remove duplicated test.

--- tests/test_error.py
+++ tests/test_error.py
@@ -8,10 +8,3 @@ import quasardb
 def test_connect_with_trace():
     with pytest.raises(quasardb.Error, match=r"at qdb_connect"):
         quasardb.Cluster(uri='invalid_uri')
-
-
-def test_query_with_trace(qdbd_connection, table, column_name):
-    with pytest.raises(quasardb.Error) as excinfo:
-        qdbd_connection.query("select notexists from " +
-                              table.get_name() + " in range (1990, +40y)")
-    assert str(excinfo.value) == 'The timeseries does not contain this column. Could not find column \'notexists\'.'
