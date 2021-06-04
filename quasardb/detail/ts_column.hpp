f007b849c818f1904bab528f23a9901ee93df818
Leon Mergen <leon@solatis.com>
2021-06-04 14:34:31 +0200

    Ensure column type string representation is pretty

--- quasardb/detail/ts_column.hpp
+++ quasardb/detail/ts_column.hpp
@@ -39,6 +39,23 @@ namespace qdb
 namespace detail
 {
 
+std::string type_to_string (qdb_ts_column_type_t t) {
+  switch (t) {
+  case qdb_ts_column_double:
+    return "double";
+  case qdb_ts_column_blob:
+    return "blob";
+  case qdb_ts_column_int64:
+    return "int64";
+  case qdb_ts_column_timestamp:
+    return "timestamp";
+  case qdb_ts_column_string:
+    return "string";
+  default:
+    return "uninitialized";
+  }
+}
+
 struct column_info
 {
     column_info() = default;
@@ -58,6 +75,12 @@ struct column_info
     column_info(const qdb_ts_column_info_ex_t & ci)
         : column_info{ci.type, ci.name, ci.symtable}
     {}
+
+    std::string repr() const
+    {
+        return "<quasardb.ColumnInfo name='" + name + "' type='" + type_to_string(type) + "'>";
+    }
+
 
     operator qdb_ts_column_info_t() const noexcept
     {
@@ -176,6 +199,7 @@ static inline void register_ts_column(Module & m)
     py::class_<column_info>{m, "ColumnInfo"}                                             //
         .def(py::init<qdb_ts_column_type_t, const std::string &>())                      //
         .def(py::init<qdb_ts_column_type_t, const std::string &, const std::string &>()) //
+        .def("__repr__", &column_info::repr)
         .def_readwrite("type", &column_info::type)                                       //
         .def_readwrite("name", &column_info::name)                                       //
         .def_readwrite("symtable", &column_info::symtable);                              //
