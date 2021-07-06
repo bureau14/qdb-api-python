8cda2fb96f065c640dd2433790539c8148f5d944
vianney <vianney.p@outlook.com>
2021-07-06 06:14:17 +0200

    Add tarball to linux build.

--- scripts/teamcity/10.build.sh
+++ scripts/teamcity/10.build.sh
@@ -3,5 +3,14 @@
 PYTHON="${PYTHON_CMD:-python}"
 
 rm -r -f dist/
+
+case "$(uname)" in
+    Linux*)
+        ${PYTHON} setup.py sdist -d dist/
+    ;;
+    *)
+    ;;
+esac
+
 ${PYTHON} setup.py bdist_egg -d dist/
 ${PYTHON} setup.py bdist_wheel -d dist/
