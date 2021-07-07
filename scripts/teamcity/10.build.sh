b1126a609ce9cbf5337d96c251f705b4d2a523ef
vianney <vianney.p@outlook.com>
2021-07-07 09:02:59 +0200

    Build tarball on all platforms.

--- scripts/teamcity/10.build.sh
+++ scripts/teamcity/10.build.sh
@@ -4,13 +4,6 @@ PYTHON="${PYTHON_CMD:-python}"
 
 rm -r -f dist/
 
-case "$(uname)" in
-    Linux*)
-        ${PYTHON} setup.py sdist -d dist/
-    ;;
-    *)
-    ;;
-esac
-
+${PYTHON} setup.py sdist -d dist/
 ${PYTHON} setup.py bdist_egg -d dist/
 ${PYTHON} setup.py bdist_wheel -d dist/
