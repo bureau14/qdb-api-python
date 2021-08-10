574cbc1402070b2bb95177429ba7758453a55c65
Leon Mergen <leon@solatis.com>
2021-08-10 10:18:17 +0200

    [WiP] Pool decorators

--- quasardb/pool.py
+++ quasardb/pool.py
@@ -53,3 +53,44 @@ class Pool(object):
         """
         """
         print("releasing conn: {}".format(conn))
+
+def _inject_pool_arg(arg, args, kwargs):
+    """
+    Decorator utility function. Takes the argument provided to the decorator
+    that configures how we should inject the pool into the args to the callback
+    function, and injects it in the correct place.
+    """
+    pool = 'Test'
+
+    if isinstance(arg, int):
+        # Invoked positional such as `@with_pool(arg=1)`, put the pool into the
+        # correct position.
+        #
+        # Because positional args are always a tuple, and tuples don't have an
+        # easy 'insert into position' function, we just cast to and from a list.
+        args = list(args)
+        args.insert(arg, pool)
+        args = tuple(args)
+    else:
+        assert isinstance(arg, str) == True
+        # If not a number, we assume it's a kwarg, which makes things easier
+        kwargs[arg] = pool
+
+    return (args, kwargs)
+
+
+def with_pool(_fn=None, *, arg=0):
+    def inner(fn):
+        def wrapper(*args, **kwargs):
+            print("before decorator, kwargs = {}".format(kwargs))
+            (args, kwargs) = _inject_pool_arg(arg, args, kwargs)
+            result = fn(*args, **kwargs)
+            print("after decorator")
+            return result
+
+        return wrapper
+
+    if _fn is None:
+        return inner
+    else:
+        return inner(_fn)
--- tests/test_pool.py
+++ tests/test_pool.py
@@ -1,16 +1,41 @@
 import re
 import pytest
-import quasardb
+from quasardb.pool import Pool, with_pool
 
 def test_can_create_pool():
     # Basic test whether whether we can create a new pool
 
-    with quasardb.Pool(uri='qdb://127.0.0.1:2836') as pool:
+    with Pool(uri='qdb://127.0.0.1:2836') as pool:
         pass
 
 def test_can_acquire_connection():
     # Basic test whether whether we can acquire a new connection
 
-    with quasardb.Pool(uri='qdb://127.0.0.1:2836', size=16) as pool:
+    with Pool(uri='qdb://127.0.0.1:2836') as pool:
         with pool.connect() as conn:
             pass
+
+def test_use_acquired_connection(entry_name):
+    # Ensures the connection returned by `pool.connect()` is actually a usable
+    # QuasarDB connection. It's relevant because we extend the connection class
+    # for our Pool.
+
+    with Pool(uri='qdb://127.0.0.1:2836') as pool:
+        with pool.connect() as conn:
+            conn.integer(entry_name).put(1234)
+            assert conn.integer(entry_name).get() == 1234
+
+
+@with_pool
+def _test_default_decorator_impl(pool):
+    return True
+
+def test_default_decorator():
+    assert _test_default_decorator_impl() == True
+
+@with_pool(arg='foo')
+def _test_kwargs_decorator_impl(entry_name, foo):
+    return True
+
+def test_kwarg_decorator(entry_name):
+    assert _test_kwargs_decorator_impl(entry_name) == True
