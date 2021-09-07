526b024cbf1d821c033721e0ad88bdad79554b87
Leon Mergen <leon@solatis.com>
2021-09-07 11:25:48 +0200

    Ability to calculate statistic deltas

--- quasardb/stats.py
+++ quasardb/stats.py
@@ -1,4 +1,5 @@
 import re
+import copy
 import quasardb
 import logging
 from datetime import datetime
@@ -62,6 +63,80 @@ def of_node(conn, uri):
 
     ret['cumulative']['check.online'] = 1
     ret['cumulative']['check.duration_ms'] = int(check_duration.total_seconds() * 1000)
+
+    return ret
+
+_stat_types = {'requests.bytes_out': 'counter',
+               'requests.bytes_in': 'counter',
+               'async_pipelines.merge.bucket_count': 'counter',
+               'async_pipelines.merge.duration_us': 'counter',
+               'async_pipelines.write.successes_count': 'counter',
+               'async_pipelines.write.failures_count': 'counter',
+               'async_pipelines.write.time_us': 'counter'}
+
+async_pipeline_bytes_pattern  = re.compile(r'async_pipelines.pipe_[0-9]+.merge_map.bytes')
+async_pipeline_count_pattern  = re.compile(r'async_pipelines.pipe_[0-9]+.merge_map.count')
+
+def _stat_type(stat_id):
+    """
+    Returns the statistic type by a stat id. Returns one of:
+
+     - 'gauge'
+     - 'counter'
+
+    This is useful for determining which value should be reported in a dashboard.
+    """
+    if stat_id in _stat_types:
+        return _stat_types[stat_id]
+    elif stat_id.endswith('total_ns'):
+        return 'counter'
+    elif async_pipeline_bytes_pattern.match(stat_id):
+        return 'gauge'
+    elif async_pipeline_count_pattern.match(stat_id):
+        return 'gauge'
+    else:
+        print("unrecognized: {}".format(stat_id))
+        return None
+
+
+
+def _calculate_delta_stat(stat_id, prev, cur):
+    logger.info("calculating delta for stat_id = {}, prev = {}. cur = {}".format(stat_id, prev, cur))
+
+    stat_type = _stat_type(stat_id)
+    if stat_type == 'counter':
+        return cur - prev
+    elif stat_type == 'gauge':
+        return cur
+    else:
+        return None
+
+def _calculate_delta_stats(prev_stats, cur_stats):
+    ret = {}
+    for stat_id in cur_stats.keys():
+        try:
+            prev_stat = cur_stats[stat_id]
+            cur_stat  = cur_stats[stat_id]
+
+            value = _calculate_delta_stat(stat_id, prev_stat, cur_stat)
+            if value is not None:
+                ret[stat_id] = value
+
+        except KeyError:
+            # Stat likely was not present yet in prev_stats
+            pass
+
+    return ret
+
+
+def calculate_delta(prev, cur):
+    """
+    Calculates the 'delta' between two successive statistic measurements.
+    """
+    ret = {}
+    for node_id in cur.keys():
+        ret[node_id] = _calculate_delta_stats(prev[node_id]['cumulative'],
+                                              cur[node_id]['cumulative'])
 
     return ret
 
