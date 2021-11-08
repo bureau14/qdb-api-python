f9f6ab437a2a95ecdd414a6ab5be4e47c041ca2d
Leon Mergen <leon@solatis.com>
2021-11-08 16:35:33 +0100

    Build Linux using manylinux

--- scripts/teamcity/10.build.sh
+++ scripts/teamcity/10.build.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
-PYTHON="${PYTHON_CMD:-python}"
+set -e -u -x
+
+function relabel_wheel {
+    wheel="$1"
+
+    if ! auditwheel show "$wheel"
+    then
+        echo "Skipping non-platform wheel $wheel"
+    else
+        # ${AUDITWHEEL_PLAT} is defined in manylinux base docker image
+        auditwheel repair "$wheel" --plat "$AUDITWHEEL_PLAT" -w dist/
+    fi
+}
+
+PYTHON="${PYTHON_CMD:-python3}"
 
 rm -r -f dist/
 
 ${PYTHON} setup.py sdist -d dist/
 ${PYTHON} setup.py bdist_egg -d dist/
 ${PYTHON} setup.py bdist_wheel -d dist/
+
+for whl in dist/*.whl; do
+    relabel_wheel "$whl"
+done
