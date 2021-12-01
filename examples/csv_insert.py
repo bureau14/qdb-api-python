693fced88c72417f8069f4272693dd664a044353
Leon Mergen <leon@solatis.com>
2021-12-01 17:42:20 +0100

    Update copyright year.

--- examples/csv_insert.py
+++ examples/csv_insert.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2009-2020, quasardb SAS. All rights reserved.
+# Copyright (c) 2009-2021, quasardb SAS. All rights reserved.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
