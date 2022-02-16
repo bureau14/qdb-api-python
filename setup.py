d86cfcf2b562a3420f5569d4cfe4faa17640242b
Leon Mergen <leon@solatis.com>
2022-02-16 13:44:41 +0100

    Bump version to 3.13.3-nightly.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from distutils.version import LooseVersion
 from pkg_resources import get_build_platform
 from wheel.bdist_wheel import bdist_wheel as old_bdist_wheel
 
-qdb_version = "3.13.2.dev0"
+qdb_version = "3.13.3.dev0"
 
 # package_modules are our 'extra' files. Our cmake configuration copies our QDB_API_LIB
 # into our source directory, and by adding this to `package_modules` we tell setuptools to
