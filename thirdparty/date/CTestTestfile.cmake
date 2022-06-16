d4a74b3797bdc9522ff62ac9a500d377fb390976
Leon Mergen <leon@solatis.com>
2022-06-16 18:16:53 +0200

    Add `date`

---
+++ thirdparty/date/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /var/home/lmergen/git/qdb-api-python/thirdparty/date
+# Build directory: /var/home/lmergen/git/qdb-api-python/thirdparty/date
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
