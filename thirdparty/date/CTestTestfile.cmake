e81bd790a67e3991e2d78119a9b63ae3720a2741
Leon Mergen <leon@solatis.com>
2022-06-17 13:23:32 +0200

    Cleanup

--- thirdparty/date/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /var/home/lmergen/git/qdb-api-python/thirdparty/date
-# Build directory: /var/home/lmergen/git/qdb-api-python/thirdparty/date
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
