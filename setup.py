f92795759eabc934498047dbc893e451aedc59cb
Leon Mergen <leon@solatis.com>
2022-08-18 15:06:59 +0200

    Mark release 3.13.1.post1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools.command.install import install
 from pkg_resources import get_build_platform
 from wheel.bdist_wheel import bdist_wheel as old_bdist_wheel
 
-qdb_version = "3.13.6.dev0"
+qdb_version = "3.13.1.post1"
 
 # package_modules are our 'extra' files. Our cmake configuration copies our QDB_API_LIB
 # into our source directory, and by adding this to `package_modules` we tell setuptools to
