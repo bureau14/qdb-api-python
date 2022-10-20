ce51492e39429699fee272d040e6b7664b0ead55
vianney <vianney.p@outlook.com>
2022-10-20 16:36:36 +0200

    Set all python cmake args.

--- setup.py
+++ setup.py
@@ -118,7 +118,7 @@ class CMakeBuild(build_ext):
         if platform.system() == "Windows":
             cmake_args += [
                 '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
-            cmake_args += ['-DPython_ROOT_DIR=C:\Python3.8-64', '-DPYTHON_LIBRARY=C:\Python3.8-64\libs/python38.lib']
+            cmake_args += ['-DPYTHON_EXECUTABLE:FILEPATH=C:\Python3.8-64\python.exe', '-DPYTHON_LIBRARY=C:\Python3.8-64\libs/python38.lib', '-DPYTHON_LIBRARIES=C:\Python3.8-64\libs/python38.lib']
             cmake_args += ['-T', 'host=x64']
             if sys.maxsize > 2**32:
                 cmake_args += ['-A', 'x64']
@@ -138,6 +138,8 @@ class CMakeBuild(build_ext):
             '-DRAPIDJSON_BUILD_TESTS=OFF',
             '-DRAPIDJSON_BUILD_THIRDPARTY_GTEST=OFF',
         ]
+
+        print("___ CMAKE ARGS: ", ' '.join(cmake_args))
 
         env = os.environ.copy()
 
