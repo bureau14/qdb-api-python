3e3dcc9a54764eacc5317bc88e8638f99a5eb266
vianney <vianney.p@outlook.com>
2022-10-20 14:43:05 +0200

    Set python root dir on windows.

--- setup.py
+++ setup.py
@@ -118,6 +118,7 @@ class CMakeBuild(build_ext):
         if platform.system() == "Windows":
             cmake_args += [
                 '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
+            cmake_args += ['-DPython_ROOT_DIR="C:\Python3.8-64"']
             cmake_args += ['-T', 'host=x64']
             if sys.maxsize > 2**32:
                 cmake_args += ['-A', 'x64']
