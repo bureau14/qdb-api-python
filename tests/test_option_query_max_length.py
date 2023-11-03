3d030c26dc9331c51a0270d778a23da315676fdf
Leon Mergen <leon@solatis.com>
2023-11-03 08:50:36 +0700

    Fix max query length tests, group all option tests into a single module

--- tests/test_option_query_max_length.py
+++
@@ -1,9 +0,0 @@
-# pylint: disable=C0103,C0111,C0302,W0212
-
-import quasardb
-import pytest
-
-
-def test_query_max_length(qdbd_connection):
-    qdbd_connection.options().set_max_query_length(1234)
-    assert qdbd_connection.options().get_max_query_length() == 1234
