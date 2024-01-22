31147f37988a60ebc87614c92ba5d2bbfca4a2cd
terngkub <terngnattapol@gmail.com>
2024-01-23 01:19:41 +0700

    Update copyright year.

--- quasardb/__init__.py
+++ quasardb/__init__.py
@@ -1,6 +1,6 @@
 # pylint: disable=C0103,C0111,C0302,R0903
 
-# Copyright (c) 2009-2023, quasardb SAS. All rights reserved.
+# Copyright (c) 2009-2024, quasardb SAS. All rights reserved.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
