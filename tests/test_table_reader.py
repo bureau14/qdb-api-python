2fcc42f2f571cf4f3afb30923b48d5bf914bb36e
Leon Mergen <leon@solatis.com>
2024-07-21 13:32:18 +0700

    Fix coding style

--- tests/test_table_reader.py
+++ tests/test_table_reader.py
@@ -1,8 +1,6 @@
 # pylint: disable=C0103,C0111,C0302,W0212
 import pytest
 import quasardb
-from functools import reduce
-import test_batch_inserter as batchlib
 import numpy as np
 
 
@@ -19,7 +17,7 @@ def test_cannot_iterate_reader_without_open(qdbd_connection, table):
     reader = qdbd_connection.reader(table_names)
 
     with pytest.raises(quasardb.UninitializedError):
-        rows = list(reader)
+        _ = list(reader)
 
 
 def test_reader_has_infinite_batch_size_by_default(qdbd_connection, table):
@@ -47,7 +45,6 @@ def test_can_set_batch_size_as_kwarg(qdbd_connection, table):
 def test_reader_returns_dicts(qdbpd_write_fn, df_with_table, qdbd_connection):
     (ctype, dtype, df, table) = df_with_table
 
-    column_names = list(column.name for column in table.list_columns())
     qdbpd_write_fn(df, qdbd_connection, table, infer_types=False, dtype=dtype)
 
     table_names = [table.get_name()]
@@ -58,17 +55,21 @@ def test_reader_returns_dicts(qdbpd_write_fn, df_with_table, qdbd_connection):
             assert isinstance(row, dict)
 
             # Ensure each key is a numpy array-like
-            for k,v in row.items():
+            for k, v in row.items():
 
                 if k == '$timestamp':
-                    # this is the timestamp index, should always be a regular numpy array
+                    # this is the timestamp index, should always be a regular
+                    # numpy array
                     assert isinstance(v, np.ndarray)
                 else:
                     assert isinstance(v, np.ma.core.MaskedArray)
 
 
-
-def test_reader_can_iterate_rows(qdbpd_write_fn, df_with_table, qdbd_connection, row_count):
+def test_reader_can_iterate_rows(
+        qdbpd_write_fn,
+        df_with_table,
+        qdbd_connection,
+        row_count):
     (ctype, dtype, df, table) = df_with_table
 
     assert len(df.index) == row_count
@@ -83,9 +84,10 @@ def test_reader_can_iterate_rows(qdbpd_write_fn, df_with_table, qdbd_connection,
         seen = False
 
         for row in reader:
-            # When we're not providing a "batch size", we expect everything in 1 large batch, and since
-            # we only have 1 table, we should really just have only a single iteration.
-            assert seen == False
+            # When we're not providing a "batch size", we expect everything in
+            # 1 large batch, and since we only have 1 table, we should really
+            # just have only a single iteration.
+            assert seen is False
             seen = True
 
             assert len(row['$timestamp']) == row_count
@@ -94,16 +96,19 @@ def test_reader_can_iterate_rows(qdbpd_write_fn, df_with_table, qdbd_connection,
                 assert len(row[column_name]) == row_count
 
 
-def test_reader_can_iterate_batches(qdbpd_write_fn, df_with_table, qdbd_connection, row_count):
+def test_reader_can_iterate_batches(
+        qdbpd_write_fn,
+        df_with_table,
+        qdbd_connection,
+        row_count):
     (ctype, dtype, df, table) = df_with_table
 
     assert len(df.index) == row_count
 
-    # We expect the row_count to be divisble by 2, otherwise the test below doesn't work anymore
+    # We expect the row_count to be divisble by 2, otherwise the test below
+    # doesn't work anymore
     assert row_count % 2 == 0
     batch_size = int(row_count / 2)
-
-
     column_names = list(column.name for column in table.list_columns())
 
     qdbpd_write_fn(df, qdbd_connection, table, infer_types=False, dtype=dtype)
