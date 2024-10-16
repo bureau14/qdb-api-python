51676c82a34b04715ce7718d12bf416c080d19b6
Leon Mergen <leon@solatis.com>
2024-10-16 18:43:51 +0700

    QDB-14765 - Add support for encrypted connections (#71)
    This is band-aid until the C API supports automatic detection of encryption or not.

--- tests/conftest.py
+++ tests/conftest.py
@@ -74,6 +74,7 @@ def qdbd_secure_connection(qdbd_settings):
         user_name=qdbd_settings.get("security").get("user_name"),
         user_private_key=qdbd_settings.get("security").get("user_private_key"),
         cluster_public_key=qdbd_settings.get("security").get("cluster_public_key"),
+        enable_encryption=True,
     )
     conn.purge_all(datetime.timedelta(minutes=1))
     yield conn
@@ -92,6 +93,7 @@ def qdbd_direct_connection(request):
             user_name=settings.get("security").get("user_name"),
             user_private_key=settings.get("security").get("user_private_key"),
             cluster_public_key=settings.get("security").get("cluster_public_key"),
+            enable_encryption=True,
         )
 
 
