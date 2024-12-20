c83c361d0788b88682a8fb71ad008c7bb7d3124a
Leon Mergen <leon@solatis.com>
2024-12-20 16:30:07 +0700

    QDB-15011 - Flaky python tests due to setuptools (#77)
    Setuptools' built-in integration of testing has been deprecated; this bug started surfacing more and more with recent versions of Python.
    
    Since Python 3.12, distutils officially has officially been deprecated, and setuptools still ships with a shadow copy of it, but they're slowly removing this.
    
    This patch moves some things around. Specifically:
    - rather than running python3 setup.py test, we now invoke python3 -m pytest directly;
    - we are super careful to execute all commands inside a virtualenv, which did not work properly before on windows;
    to avoid conflicts between the directory quasardb and the installed quasardb module, we execute pytest in the tests/ subdirectory;
    - we now use the build python module to handle the building of the python api for us.
    
    Still left to-do:
    - switch documentation backend, it's deprecated and doesn't work properly anymore;

--- scripts/teamcity/30.doc.sh
+++ scripts/teamcity/30.doc.sh
@@ -4,8 +4,33 @@ SCRIPT_DIR="$(cd "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 
 source ${SCRIPT_DIR}/00.common.sh
 
-python3 -m pip install dist/quasardb*.whl
-python3 -m pip install -r dev-requirements.txt
-mkdir doc || true
-python3 docgen.py
+set -u -x
+
+PYTHON="${PYTHON_CMD:-python3}"
+
+${PYTHON} -m venv --clear ${SCRIPT_DIR}/../../.env/
+if [[ "$(uname)" == MINGW* ]]
+then
+    VENV_PYTHON="${SCRIPT_DIR}/../../.env/Scripts/python.exe"
+else
+    VENV_PYTHON="${SCRIPT_DIR}/../../.env/bin/python"
+fi
+
+${VENV_PYTHON} -m pip install -r dev-requirements.txt
+${VENV_PYTHON} -m pip install --no-deps --force-reinstall dist/quasardb-*.whl
+
+
+# To avoid conflicts with `quasardb` directory and `import quasardb`
+rm -rf tmp || true
+rm -rf doc || true
+
+mkdir tmp
+pushd tmp
+
+${VENV_PYTHON} ../docgen.py
+
+popd
+
+mv -v tmp/doc doc
+
 tar -czvf dist/doc.tar.gz doc/*
