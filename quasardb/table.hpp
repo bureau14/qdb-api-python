e4125d29354613ffe672b1803b61a7849268e5d8
Leon Mergen <leon@solatis.com>
2025-01-31 12:44:50 +0700

    Fix copyright

--- quasardb/table.hpp
+++ quasardb/table.hpp
@@ -2,7 +2,7 @@
  *
  * Official Python API
  *
- * Copyright (c) 2009-2024, quasardb SAS. All rights reserved.
+ * Copyright (c) 2009-2025, quasardb SAS. All rights reserved.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
