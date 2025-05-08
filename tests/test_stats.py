6f0f96be480d2deb52313e8c10dca9980923e108
Leon Mergen <leon@solatis.com>
2025-05-08 10:50:27 +0700

    QDB-16178 - Refactor python statistics module (#93)
    Makes use of the new server-side metric type/units, and changes the way statistics are exposed. 
    
    This is a breaking change for users who previously used the statistics API.

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -20,16 +20,38 @@ def _write_data(conn, table):
     )
 
 
-def _ensure_stats(conn, table):
-    global _has_stats
+def _has_stats(conn):
+    xs = qdbst.by_node(conn)
 
+    # As we always use a secure connection, we will wait for by-uid statistics to appear.
+    #
+    # We also don't expect a cluster to be running, so just ensure we have exactly 1 node.
+    ks = list(xs.keys())
+    assert len(ks) == 1
+
+    node_id = ks[0]
+    node_stats = xs[node_id]
+
+    assert "by_uid" in node_stats
+    uid_stats = node_stats["by_uid"]
+
+    # The actual check happens here: we expect at least 1 per-uid statistic
+    return len(uid_stats.keys()) > 0
+
+
+def _ensure_stats(conn, table):
     # This function is merely here to generate some activity on the cluster,
     # so that we're guaranteed to have some statistics
-    if _has_stats is False:
+
+    max_polls = 10
+    n = 0
+
+    while _has_stats(conn) is False:
         _write_data(conn, table)
-        # Statistics refresh interval is 5 seconds.
-        sleep(6)
-        _has_stats = True
+        sleep(1)
+
+        n = n + 1
+        assert n <= max_polls
 
 
 # A fairly conservative set of user-specific stats we always expect after doing our
@@ -57,6 +79,25 @@ _expected_cumulative_stats = [
 ]
 
 
+def _validate_stats_dict(xs):
+    """
+    Validates a dict with keys / stat tuples association.
+    """
+    for k, x in xs.items():
+        print("k: ", k, ", x: ", x)
+
+        # Each statistic is a dict of type/unit/value
+        assert isinstance(x, dict)
+        assert "value" in x
+        assert "type" in x
+        assert "unit" in x
+
+        # Everything that's not a NONE unit (i.e. not a label) should be an int
+        if x["unit"] != qdbst.Unit.NONE:
+
+            assert isinstance(x["value"], int)
+
+
 def _validate_node_stats(stats):
     assert "by_uid" in stats
     assert "cumulative" in stats
@@ -68,19 +109,19 @@ def _validate_node_stats(stats):
         for expected in _expected_user_stats:
             assert expected in xs
 
-        for _, v in xs.items():
-            # As far as I know, per-user statistics should *always* be
-            # integers
-            assert isinstance(v, int)
+        _validate_stats_dict(xs)
 
     # Test cumulative stats
     xs = stats["cumulative"]
     for expected in _expected_cumulative_stats:
         assert expected in xs
 
+        _validate_stats_dict(xs)
+
 
 def test_stats_by_node(qdbd_secure_connection, secure_table):
     _ensure_stats(qdbd_secure_connection, secure_table)
+
     xs = qdbst.by_node(qdbd_secure_connection)
 
     assert len(xs) == 1
@@ -91,7 +132,7 @@ def test_stats_by_node(qdbd_secure_connection, secure_table):
 
 def test_stats_of_node(qdbd_settings, qdbd_secure_connection, secure_table):
     # First seed the table
-    # _ensure_stats(qdbd_secure_connection, secure_table)
+    _ensure_stats(qdbd_secure_connection, secure_table)
 
     # Now establish direct connection
     conn = quasardb.Node(
@@ -108,6 +149,7 @@ def test_stats_of_node(qdbd_settings, qdbd_secure_connection, secure_table):
 def test_stats_regex():
     # This is mostly to test a regression, where user-stats for users with multi-digit ids
     # were not picked up.
+
     user_stats = [
         "$qdb.statistics.foo.uid_1",
         "$qdb.statistics.foo.uid_21",
@@ -127,3 +169,8 @@ def test_stats_regex():
     for s in total_stats:
         assert qdbst.is_user_stat(s) is False
         assert qdbst.is_cumulative_stat(s) is True
+
+
+def test_stat_type_is_deprecated():
+    with pytest.deprecated_call():
+        qdbst.stat_type("foobar")
