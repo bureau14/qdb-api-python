868a78ded6cf1ded2cb55424e0c69d1c7b5b7f71
vikonix <valeriy.kovalev.software@gmail.com>
2025-12-04 19:00:40 +0100

    more tests

--- tests/test_arrow_batch_push.py
+++ tests/test_arrow_batch_push.py
@@ -4,15 +4,30 @@ import pytest
 import quasardb
 
 
+def _arrow_reader(timestamps, values):
+    pa = pytest.importorskip("pyarrow")
+
+    ts_array = pa.array(timestamps.astype("datetime64[ns]"), type=pa.timestamp("ns"))
+    value_array = pa.array(values, type=pa.float64())
+    batch = pa.record_batch([ts_array, value_array], names=["$timestamp", "value"])
+    return pa.RecordBatchReader.from_batches(batch.schema, [batch])
+
+
+def _create_arrow_table(connection, entry_name):
+    table_name = entry_name + "_arrow"
+    table = connection.table(table_name)
+
+    column = quasardb.ColumnInfo(quasardb.ColumnType.Double, "value")
+    table.create([column])
+
+    return table
+
+
 @pytest.mark.usefixtures("qdbd_connection")
 def test_batch_push_arrow_with_options(qdbd_connection, entry_name):
     pa = pytest.importorskip("pyarrow")
 
-    table_name = entry_name + "_arrow"
-    table = qdbd_connection.table(table_name)
-
-    column = quasardb.ColumnInfo(quasardb.ColumnType.Double, "value")
-    table.create([column])
+    table = _create_arrow_table(qdbd_connection, entry_name)
 
     timestamps = np.array(
         [
@@ -28,7 +43,7 @@ def test_batch_push_arrow_with_options(qdbd_connection, entry_name):
     reader = pa.RecordBatchReader.from_batches(batch.schema, [batch])
 
     qdbd_connection.batch_push_arrow(
-        table_name,
+        table.get_name(),
         reader,
         deduplicate=False,
         deduplication_mode="drop",
@@ -42,3 +57,61 @@ def test_batch_push_arrow_with_options(qdbd_connection, entry_name):
 
     np.testing.assert_array_equal(results[0], timestamps)
     np.testing.assert_allclose(results[1], np.array([1.5, 2.5]))
+
+
+@pytest.mark.parametrize("deduplication_mode, expected_values", [
+    ("drop", np.array([1.5, 2.5])),
+    ("upsert", np.array([10.5, 11.5])),
+])
+def test_batch_push_arrow_deduplicate_modes(
+    qdbd_connection, entry_name, deduplication_mode, expected_values
+):
+    pytest.importorskip("pyarrow")
+
+    table = _create_arrow_table(qdbd_connection, entry_name)
+
+    timestamps = np.array(
+        [
+            np.datetime64("2024-01-01T00:00:00", "ns"),
+            np.datetime64("2024-01-01T00:00:01", "ns"),
+        ],
+        dtype="datetime64[ns]",
+    )
+
+    initial_reader = _arrow_reader(timestamps, [1.5, 2.5])
+    duplicate_reader = _arrow_reader(timestamps, [10.5, 11.5])
+
+    qdbd_connection.batch_push_arrow(table.get_name(), initial_reader, write_through=True,)
+    qdbd_connection.batch_push_arrow(
+        table.get_name(), duplicate_reader, deduplicate=True, deduplication_mode=deduplication_mode, write_through=True,
+
+    )
+
+    results = table.double_get_ranges(
+        "value", [(timestamps[0], timestamps[-1] + np.timedelta64(1, "s"))]
+    )
+
+    np.testing.assert_array_equal(results[0], timestamps)
+    np.testing.assert_allclose(results[1], expected_values)
+
+
+def test_batch_push_arrow_invalid_deduplication_mode(qdbd_connection, entry_name):
+    pytest.importorskip("pyarrow")
+
+    table = _create_arrow_table(qdbd_connection, entry_name)
+
+    timestamps = np.array(
+        [
+            np.datetime64("2024-01-01T00:00:00", "ns"),
+            np.datetime64("2024-01-01T00:00:01", "ns"),
+        ],
+        dtype="datetime64[ns]",
+    )
+
+    reader = _arrow_reader(timestamps, [1.5, 2.5])
+
+    with pytest.raises(quasardb.InvalidArgumentError):
+        qdbd_connection.batch_push_arrow(
+            table.get_name(), reader, deduplicate=True, deduplication_mode="invalid", write_through=False,
+
+        )
