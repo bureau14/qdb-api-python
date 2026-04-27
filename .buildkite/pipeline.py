6bd48c350f21abb44a4c77bf130f5cf4940bc787
Igor <igor.niebylski@gmail.com>
2026-04-27 12:15:20 +0200

    ready

--- .buildkite/pipeline.py
+++ .buildkite/pipeline.py
@@ -41,9 +41,9 @@ PLATFORMS: list[Platform] = [
     dataclasses.replace(p, **_OS_OVERLAY.get(p.os, {}))
     for p in select_platforms(
         "linux-amd64-core2",
-        # "linux-aarch64",
-        # "windows-amd64-core2",
-        # "macos-aarch64",
+        "linux-aarch64",
+        "windows-amd64-core2",
+        "macos-aarch64",
     )
 ]
 
@@ -156,7 +156,7 @@ def generate_pipeline() -> Pipeline:
                 artifact_vars_per_step = {
                     "upload": {"variant": slug, "git-ref": git_ref},
                     "promote": {"variant": slug, "git-ref": git_ref},
-                    "download": {"variant": dependency_slug, "git-ref": "refs/heads/sc-18547/buildkite"},
+                    "download": {"variant": dependency_slug, "git-ref": git_ref},
                 }
 
                 compose_config = {
