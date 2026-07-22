c9c944584fbec0fa169bed91c7dbce18210641f5
vikonix <47449932+vikonix@users.noreply.github.com>
2026-07-22 10:56:17 +0200

    QDB-15924 - code formatting (#126)
    QDB-15924 - code formatting (#126)

--- quasardb/cluster.hpp
+++ quasardb/cluster.hpp
@@ -61,7 +61,6 @@
 #include <chrono>
 #include <iostream>
 
-
 namespace qdb
 {
 
@@ -337,8 +336,8 @@ public:
     {
         check_open();
 
-	auto o = std::make_shared<qdb::find_query>(_handle, query_string);
-	return o->run();
+        auto o = std::make_shared<qdb::find_query>(_handle, query_string);
+        return o->run();
     }
 
     py::object query(const std::string & query_string, const py::object & blobs)
@@ -502,23 +501,29 @@ public:
     {
         check_open();
 
-        std::string query = query_string;
+        std::string query              = query_string;
         const std::string limit_string = "LIMIT 1";
         query += " " + limit_string;
-      
+
         // TODO:
         // should return dict of column names and dtypes
         // currently returns numpy masked arrays
         return py::cast(qdb::numpy_query(_handle, query));
     }
 
-    py::object split_query_range(std::chrono::system_clock::time_point start, std::chrono::system_clock::time_point end, std::chrono::milliseconds delta)
+    py::object split_query_range(std::chrono::system_clock::time_point start,
+        std::chrono::system_clock::time_point end,
+        std::chrono::milliseconds delta)
     {
-        std::vector<std::pair<std::chrono::system_clock::time_point, std::chrono::system_clock::time_point>> ranges;
+        std::vector<
+            std::pair<std::chrono::system_clock::time_point, std::chrono::system_clock::time_point>>
+            ranges;
 
-        for (auto current_start = start; current_start < end; ) {
+        for (auto current_start = start; current_start < end;)
+        {
             auto current_end = current_start + delta;
-            if (current_end > end) {
+            if (current_end > end)
+            {
                 current_end = end;
             }
             ranges.emplace_back(current_start, current_end);
@@ -526,7 +531,6 @@ public:
         }
         return py::cast(ranges);
     }
-
 
 private:
     std::string _uri;
